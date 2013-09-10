0642a9e3fc407b72cbf802fcff7a44781dd44d58
Jeff Painter <painter1@llnl.gov>
2013-09-10 15:42:27 -0700

    - Improved function list_variables for GUI menu; diagnostic specification arguments are allowed, but ignored at the moment - cnanged a couple of names - first draft of AMWG plot_set2 implementation; togther with more general-purpose code in the plot_set class

--- src/python/frontend/uvcdat.py
+++ src/python/frontend/uvcdat.py
@@ -10,10 +10,13 @@ from metrics.computation.reductions import *
 from metrics.amwg.derivations.vertical import *
 from metrics.amwg.plot_data import plotspec, derived_var
 from metrics.frontend.version import version
+from metrics.amwg.derivations import *
 from pprint import pprint
 import cProfile
 import vcs
-x=vcs.init()   # This belongs in one of the GUI files, e.g.diagnosticsDockWidget.py
+vcsx=vcs.init()   # This belongs in one of the GUI files, e.g.diagnosticsDockWidget.py
+                  # The GUI probably will have already called vcs.init().
+                  # Then, here,  'from foo.bar import vcsx'
 
 def setup_filetable( search_path, cache_path, ftid=None, search_filter=None ):
     """Returns a file table (an index which says where you can find a variable) for files in the
@@ -58,9 +61,14 @@ def clear_filetable( search_path, cache_path, search_filter=None ):
     if os.path.isfile(cache_path):
         os.remove(cache_path)
 
-def list_variables( filetable1, filetable2=None ):
+def list_variables( filetable1, filetable2=None, diagnostic_group="AMWG", diagnostic_set="" ):
     """returns a sorted list of variable ids (strings) found in both filetables provided
-    You can provide one of two filetables."""
+    You can provide one of two filetables.  You also can provide a diagnostic group and set, e.g.
+    "AMWG and "plot_set3". At the moment these is ignored, but in the future the returned variable
+    list will be limited to those which work with the selected diagnostic.
+    This is meant an aid in writing menus.  However it only tells you about the original variables,
+    i.e. what's in the data files.  It doesn't tell you about what derived data could be computed.
+    """
     if filetable1 is None: return []
     vars1 = filetable1.list_variables()
     if filetable2 is None: return vars1
@@ -76,24 +84,24 @@ class uvc_plotspec():
     The plots will be of the type specified by presentation.  The data will be the
     variable(s) supplied, and their axes.  Optionally one may specify a list of labels
     for the variables, and a title for the whole plot."""
-    # re prsentation (plottype): Yvsx is a line plot, for Y=Y(X).  It can have one or several lines.
+    # re prsentation (plottype): Yxvsx is a line plot, for Y=Y(X).  It can have one or several lines.
     # Isofill is a contour plot.  To make it polar, set projection=polar.  I'll
     # probably communicate that by passing a name "Isofill_polar".
     def __init__( self, vars, presentation, labels=[], title=''):
         type = presentation
-        if presentation=="Yvsx":
-            self.presentation = x.createyxvsx()
+        if presentation=="Yxvsx":
+            self.presentation = vcsx.createyxvsx()
             type="Yxvsx"
         elif presentation == "Isofill":
-            self.presentation = x.createisofill()
+            self.presentation = vcsx.createisofill()
         elif presentation == "Vector":
-            self.presentation = x.createvector()
+            self.presentation = vcsx.createvector()
         elif presentation == "Boxfill":
-            self.presentation = x.createboxfill()
+            self.presentation = vcsx.createboxfill()
         elif presentation == "Isoline":
-            self.presentation = x.createisoline()
+            self.presentation = vcsx.createisoline()
         ## elif presentation == "":
-        ##     self.resentation = x.create
+        ##     self.resentation = vcsx.create
         self.vars = vars
         self.labels = labels
         self.title = title
@@ -127,6 +135,8 @@ def _get_plot_data( plot_set, filetable1, filetable2, variable, season ):
     if season=='ANN':
         # cdutil.times.getMonthIndex() (called by climatology()) doesn't recognize 'ANN'
         season='JFMAMJJASOND'
+    if plot_set=='2':
+        return plot_set2( filetable1, filetable2, variable )
     if plot_set=='3':
         return plot_set3( filetable1, filetable2, variable, season )
     elif plot_set=='4':
@@ -137,7 +147,7 @@ def _get_plot_data( plot_set, filetable1, filetable2, variable, season ):
         print "ERROR, plot set",plot_set," not implemented yet!"
         return None
 
-class one_line_plot( plotspec ):
+class basic_one_line_plot( plotspec ):
     def __init__( self, yvar, xvar=None ):
         # xvar, yvar should be the actual x,y of the plot.
         # xvar, yvar should already have been reduced to 1-D variables.
@@ -145,15 +155,15 @@ class one_line_plot( plotspec ):
         if xvar is None:
             xvar = yvar.getAxisList()[0]
         plotspec.__init__( self, xvars=[xvar], yvars=[yvar],
-                           vid = yvar.id+" line plot", plottype='Yvsx' )
+                           vid = yvar.id+" line plot", plottype='Yxvsx' )
 
-class two_line_plot( plotspec ):
+class basic_two_line_plot( plotspec ):
     def __init__( self, y1var, y2var, x1var=None, x2var=None ):
         """x?var, y?var should be the actual x,y of the plots.
         x?var, y?var should already have been reduced to 1-D variables.
         Normally y?=y(x?), x? is the axis of y?."""
         plotspec.__init__( self, y1vars=[y1var], y2vars=[y2var],
-                           vid = y1var.variableid+y2var.variableid+" line plot", plottype='Yvsx' )
+                           vid = y1var.variableid+y2var.variableid+" line plot", plottype='Yxvsx' )
 
 class one_line_diff_plot( plotspec ):
     def __init__( self, y1var, y2var, vid ):
@@ -165,7 +175,7 @@ class one_line_diff_plot( plotspec ):
             yvars=[y1var,y2var],
             yfunc=aminusb_1ax,   # aminusb_1ax(y1,y2)=y1-y2; each y has 1 axis, use min axis
             vid=vid,
-            plottype='Yvsx' )
+            plottype='Yxvsx' )
 
 class contour_plot( plotspec ):
     def __init__( self, zvar, xvar=None, yvar=None, ya1var=None,
@@ -219,6 +229,9 @@ class plot_set():
         self.reduced_variables = {}
         self.derived_variables = {}
         self.variable_values = {}
+        self.single_plotspecs = {}
+        self.composite_plotspecs = {}
+        self.plotspec_values = {}
     def results(self):
         return self._results()
 # To profile, replace (by name changes) the above results() with the following one:
@@ -236,7 +249,174 @@ class plot_set():
             value = self.derived_variables[v].derive(self.variable_values)
             if value is None: return None
             self.variable_values[v] = value
+        for p,ps in self.single_plotspecs.iteritems():
+            print "jfp preparing data for",ps._id
+            xrv = [ varvals[k] for k in ps.xvars ]
+            x1rv = [ varvals[k] for k in ps.x1vars ]
+            x2rv = [ varvals[k] for k in ps.x2vars ]
+            x3rv = [ varvals[k] for k in ps.x3vars ]
+            yrv = [ varvals[k] for k in ps.yvars ]
+            y1rv = [ varvals[k] for k in ps.y1vars ]
+            y2rv = [ varvals[k] for k in ps.y2vars ]
+            y3rv = [ varvals[k] for k in ps.y3vars ]
+            yarv = [ varvals[k] for k in ps.yavars ]
+            ya1rv = [ varvals[k] for k in ps.ya1vars ]
+            zrv = [ varvals[k] for k in ps.zvars ]
+            zrrv = [ varvals[k] for k in ps.zrangevars ]
+            xax = apply( ps.xfunc, xrv )
+            x1ax = apply( ps.x1func, x1rv )
+            x2ax = apply( ps.x2func, x2rv )
+            x3ax = apply( ps.x3func, x3rv )
+            yax = apply( ps.yfunc, yrv )
+            y1ax = apply( ps.y1func, y1rv )
+            y2ax = apply( ps.y2func, y2rv )
+            y3ax = apply( ps.y3func, y3rv )
+            # not used yet yaax = apply( ps.yafunc, yarv )
+            ya1ax = apply( ps.ya1func, ya1rv )
+            zax = apply( ps.zfunc, zrv )
+            # not used yet zr = apply( ps.zrangefunc, zrrv )
+            vars = xax+x1ax+x2ax+x3ax+yax+y1ax+y2ax+y3ax+zax
+            if vars==[]:
+                continue
+            labels = []  # fix later
+            title = ""   # fix later
+            self.plotspec_values[p] = uvc_plotspec( vars, 'Yxvsx', labels, title )
+        for p,ps in self.composite_plotspecs.iteritems():
+            self.plotspec_values[p] = [ self.plotspec_values[sp] for sp in ps ]
+
         return self
+
+
+class plot_set2(plot_set):
+    """represents one plot from AMWG Diagnostics Plot Set 2
+    Each such plot is a page consisting of two to four plots.  The horizontal
+    axis is latitude and the vertical axis is heat or fresh-water transport.
+    Both model and obs data is plotted, sometimes in the same plot.
+    The data presented is averaged over everything but latitude.
+    """
+    def __init__( self, filetable1, filetable2, varid ):
+        """filetable1, filetable2 should be filetables for model and obs.
+        varid is a string identifying the derived variable to be plotted, e.g. 'Ocean_Heat'"""
+        plot_set.__init__(self)
+        self._var_baseid = '_'.join([varid,'set2'])   # e.g. Ocean_Heat_set2
+        print "In plot_set2 __init__, ignoring varid input, will compute Ocean_Heat"
+
+        # CAM variables needed for heat transport: (SOME ARE SUPERFLUOUS <<<<<<)
+        # FSNS, FLNS, FLUT, FSNTOA, FLNT, FSNT, SHFLX, LHFLX,
+        self.reduced_variables = {
+            'FSNS_1': reduced_variable(
+                variableid='FSNS',filetable=filetable1,reduction_function=(lambda x,vid:x) ),
+            'FSNS_ANN_latlon_1': reduced_variable(
+                variableid='FSNS',
+                filetable=filetable1,
+                reduction_function=reduce2latlon ),
+            'FLNS_1': reduced_variable(
+                variableid='FLNS',filetable=filetable1,reduction_function=(lambda x,vid:x) ),
+            'FLNS_ANN_latlon_1': reduced_variable(
+                variableid='FLNS',
+                filetable=filetable1,
+                reduction_function=reduce2latlon ),
+            'FLUT_ANN_latlon_1': reduced_variable(
+                variableid='FLUT',
+                filetable=filetable1,
+                reduction_function=reduce2latlon ),
+            'FSNTOA_ANN_latlon_1': reduced_variable(
+                variableid='FSNTOA',
+                filetable=filetable1,
+                reduction_function=reduce2latlon ),
+            'FLNT_1': reduced_variable(
+                variableid='FLNT',filetable=filetable1,reduction_function=(lambda x,vid:x) ),
+            'FLNT_ANN_latlon_1': reduced_variable(
+                variableid='FLNT',
+                filetable=filetable1,
+                reduction_function=reduce2latlon ),
+            'FSNT_1': reduced_variable(
+                variableid='FSNT',filetable=filetable1,reduction_function=(lambda x,vid:x) ),
+            'FSNT_ANN_latlon_1': reduced_variable(
+                variableid='FSNT',
+                filetable=filetable1,
+                reduction_function=reduce2latlon ),
+            'QFLX_1': reduced_variable(
+                variableid='QFLX',filetable=filetable1,reduction_function=(lambda x,vid:x) ),
+            'SHFLX_1': reduced_variable(
+                variableid='SHFLX',filetable=filetable1,reduction_function=(lambda x,vid:x) ),
+            'SHFLX_ANN_latlon_1': reduced_variable(
+                variableid='SHFLX',
+                filetable=filetable1,
+                reduction_function=reduce2latlon ),
+            'LHFLX_ANN_latlon_1': reduced_variable(
+                variableid='LHFLX',
+                filetable=filetable1,
+                reduction_function=reduce2latlon ),
+            'OCNFRAC_ANN_latlon_1': reduced_variable(
+                variableid='OCNFRAC',
+                filetable=filetable1,
+                reduction_function=reduce2latlon )
+            }
+        self.derived_variables = {
+            'CAM_HEAT_TRANSPORT_ALL_1': derived_var(
+                vid='CAM_HEAT_TRANSPORT_ALL_1',
+                inputs=['FSNS_ANN_latlon_1', 'FLNS_ANN_latlon_1', 'FLUT_ANN_latlon_1',
+                        'FSNTOA_ANN_latlon_1', 'FLNT_ANN_latlon_1', 'FSNT_ANN_latlon_1',
+                        'SHFLX_ANN_latlon_1', 'LHFLX_ANN_latlon_1', 'OCNFRAC_ANN_latlon_1' ],
+                outputs=['atlantic_heat_transport','pacific_heat_transport',
+                         'indian_heat_transport', 'global_heat_transport' ],
+                func=oceanic_heat_transport ),
+            'NCEP_OBS_HEAT_TRANSPORT_ALL_2': derived_var(
+                vid='NCEP_OBS_HEAT_TRANSPORT_ALL_2',
+                inputs=[],
+                outputs=('latitude', ['atlantic_heat_transport','pacific_heat_transport',
+                                      'indian_heat_transport', 'global_heat_transport' ]),
+                func=(lambda: ncep_ocean_heat_transport(path2) ) )
+            }
+        single_plotspecs = {
+            'CAM_NCEP_HEAT_TRANSPORT_GLOBAL': plotspec(
+                vid='CAM_NCEP_HEAT_TRANSPORT_GLOBAL',
+                x1vars=['FSNS_ANN_latlon_1'], x1func=latvar,
+                y1vars=['CAM_HEAT_TRANSPORT_ALL_1' ],
+                y1func=(lambda y: y[3]),
+                x2vars=['NCEP_OBS_HEAT_TRANSPORT_ALL_2'], x2func=(lambda x: x[0]),
+                y2vars=['NCEP_OBS_HEAT_TRANSPORT_ALL_2' ],
+                y2func=(lambda y: y[1][3]),
+                plottype='2 line plot'  ),
+            'CAM_NCEP_HEAT_TRANSPORT_PACIFIC': plotspec(
+                vid='CAM_NCEP_HEAT_TRANSPORT_PACIFIC',
+                x1vars=['FSNS_ANN_latlon_1'], x1func=latvar,
+                y1vars=['CAM_HEAT_TRANSPORT_ALL_1' ],
+                y1func=(lambda y: y[0]),
+                x2vars=['NCEP_OBS_HEAT_TRANSPORT_ALL_2'], x2func=(lambda x: x[0]),
+                y2vars=['NCEP_OBS_HEAT_TRANSPORT_ALL_2' ],
+                y2func=(lambda y: y[1][0]),
+                plottype='2 line plot'  ),
+            'CAM_NCEP_HEAT_TRANSPORT_ATLANTIC': plotspec(
+                vid='CAM_NCEP_HEAT_TRANSPORT_ATLANTIC',
+                x1vars=['FSNS_ANN_latlon_1'], x1func=latvar,
+                y1vars=['CAM_HEAT_TRANSPORT_ALL_1' ],
+                y1func=(lambda y: y[0]),
+                x2vars=['NCEP_OBS_HEAT_TRANSPORT_ALL_2'], x2func=(lambda x: x[0]),
+                y2vars=['NCEP_OBS_HEAT_TRANSPORT_ALL_2' ],
+                y2func=(lambda y: y[1][1]),
+                plottype='2 line plot'  ),
+            'CAM_NCEP_HEAT_TRANSPORT_INDIAN': plotspec(
+                vid='CAM_NCEP_HEAT_TRANSPORT_INDIAN',
+                x1vars=['FSNS_ANN_latlon_1'], x1func=latvar,
+                y1vars=['CAM_HEAT_TRANSPORT_ALL_1' ],
+                y1func=(lambda y: y[0]),
+                x2vars=['NCEP_OBS_HEAT_TRANSPORT_ALL_2'], x2func=(lambda x: x[0]),
+                y2vars=['NCEP_OBS_HEAT_TRANSPORT_ALL_2' ],
+                y2func=(lambda y: y[1][2]),
+                plottype='2 line plot'  )
+            }
+        composite_plotspecs = {
+            'CAM_NCEP_HEAT_TRANSPORT_ALL':
+                ['CAM_NCEP_HEAT_TRANSPORT_GLOBAL','CAM_NCEP_HEAT_TRANSPORT_PACIFIC',
+                 'CAM_NCEP_HEAT_TRANSPORT_ATLANTIC','CAM_NCEP_HEAT_TRANSPORT_INDIAN']
+            }
+
+    def _results(self):
+        results = plot_set._results(self)
+        if results is None: return None
+        return self.plotspec_values['CAM_NCEP_HEAT_TRANSPORT_ALL']
 
 
 class plot_set3(plot_set):
@@ -265,7 +445,7 @@ class plot_set3(plot_set):
             reduction_function=(lambda x,vid=None: reduce2lat_seasonal(x,season,vid=vid)) )
         self.reduced_variables[varid+'_2'] = y2var
         y2var._vid = varid+'_2'
-        self.plot_a = two_line_plot( y1var, y2var )
+        self.plot_a = basic_two_line_plot( y1var, y2var )
         vid = '_'.join([self._var_baseid,filetable1._id,filetable2._id,'diff'])
         # ... e.g. CLT_DJF_set3_CAM456_NCEP_diff
         self.plot_b = one_line_diff_plot( y1var, y2var, vid )
@@ -292,10 +472,10 @@ class plot_set3(plot_set):
                                 'diff'])
         # ... e.g. CLT_DJF_set3_CAM456_NCEP_diff
         plot_a_val = uvc_plotspec(
-            [y1val,y2val],'Yvsx', labels=[y1unam,y2unam],
+            [y1val,y2val],'Yxvsx', labels=[y1unam,y2unam],
             title=' '.join([self._var_baseid,y1unam,'and',y2unam]))
         plot_b_val = uvc_plotspec(
-            [ydiffval],'Yvsx', labels=['difference'],
+            [ydiffval],'Yxvsx', labels=['difference'],
             title=' '.join([self._var_baseid,y1unam,'-',y2unam]))
         return [ plot_a_val, plot_b_val ]
 
@@ -494,15 +674,18 @@ if __name__ == '__main__':
           filetable2 = setup_filetable(path2,os.environ['PWD'],search_filter=filt2)
       else:
           filetable2 = setup_filetable(path2,os.environ['PWD'])
+      ps2 = plot_set2( filetable1, filetable2, 'ignored' )
+      print "ps2=",ps2
+      pprint( ps.results() )
 #      ps3 = plot_set3( filetable1, filetable2, 'TREFHT', 'DJF' )
 #      print "ps3=",ps3
 #      pprint( ps3.results() )
-      ps4 = plot_set4( filetable1, filetable2, 'T', 'DJF' )
-      print "ps4=",ps4
-      pprint( ps4.results() )
-      ps5 = plot_set5( filetable1, filetable2, 'TREFHT', 'DJF' )
-      print "ps5=",ps5
-      pprint( ps5.results() )
+#      ps4 = plot_set4( filetable1, filetable2, 'T', 'DJF' )
+#      print "ps4=",ps4
+#      pprint( ps4.results() )
+#      ps5 = plot_set5( filetable1, filetable2, 'TREFHT', 'DJF' )
+#      print "ps5=",ps5
+#      pprint( ps5.results() )
    else:
       print "usage: plot_data.py root"
    """ for testing...
@@ -510,7 +693,7 @@ else:
     # My usual command-line test is:
     # ./uvcdat_interface.py /export/painter1/cam_output/*.xml ./obs_data/ filt="f_startswith('LEGATES')"
     # ...That's for plot set 3; it has no levels so it's a bad test for plot set 4.  Here's another:
-    # ./uvcdat_interface.py /export/painter1/cam_output/*.xml ./obs_data/ filt="f_startswith('NCEP')"
+    # ./uvcdat.py /export/painter1/cam_output/*.xml ./obs_data/ filt="f_startswith('NCEP')"
     path1 = '/export/painter1/cam_output/b30.009.cam2.h0.06.xml'
     path2 = '/export/painter1/metrics/src/python/obs_data/'
 #    filt2="filt=f_startswith('LEGATES')"
