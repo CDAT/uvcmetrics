84c47318eceb13d83575ecae62a7c183ac43c7e3
Jeff Painter <painter1@llnl.gov>
2013-11-04 14:41:46 -0800

    New file, a script for batch computations; works but still a work in progress.

---
+++ src/python/frontend/batch.py
@@ -0,0 +1,54 @@
+#!/usr/local/uvcdat/2013-10-30/bin/python
+
+# batch output - For now, writes data files for all plots we know how to make
+# For now, inputs are hard-coded.
+# TO DO: >>> separate plan_computation() from compute()
+# >>>> How does the class design work with this? Improve as needed.
+
+import hashlib, os, pickle, sys, os
+from metrics import *
+from metrics.io.filetable import *
+from metrics.io.findfiles import *
+from metrics.computation.reductions import *
+from metrics.amwg import *
+from metrics.amwg.derivations.vertical import *
+from metrics.amwg.plot_data import plotspec, derived_var
+from metrics.frontend.version import version
+from metrics.amwg.derivations import *
+from metrics.diagnostic_groups import *
+from pprint import pprint
+import cProfile
+
+path1 = os.path.join(os.environ["HOME"],'cam_output/b30.009.cam2.h0.06.xml')
+path2 = os.path.join(os.environ["HOME"],'obs_data')
+tmppth = os.path.join(os.environ['HOME'],"tmp")
+outpath = os.path.join(os.environ['HOME'],"tmp","diagout")
+if not os.path.exists(tmppth):
+    os.makedirs(tmppth)
+datafiles1 = dirtree_datafiles( path1 )
+filetable1 = datafiles1.setup_filetable( tmppth, "model" )
+filt2 = f_startswith("NCEP")
+datafiles2 = dirtree_datafiles( path2, filt2 )
+filetable2 = datafiles2.setup_filetable( tmppth, "obs" )
+
+number_diagnostic_plots = 0
+dm = diagnostics_menu()
+for pname,pclass in dm.items():
+    package = pclass()
+    sm = package.list_diagnostic_sets()
+    for sname,sclass in sm.items():
+        if sclass.name != ' 6- Horizontal Vector Plots of Seasonal Means':
+            continue   # for testing, only do one plot set
+        for seasonid in package.list_seasons():
+            if seasonid != 'DJF':
+                continue # for testing, only do one season
+            for varid in package.list_variables( filetable1, filetable2 ):
+                #if varid!='PS':
+                #    continue # for testing, only do one variable
+                plot = sclass( filetable1, filetable2, varid, seasonid )
+                res = plot.compute()
+                if res is not None: #>>> TO DO write res to a NetCDF file <<<<
+                    number_diagnostic_plots += 1
+                    print plot
+                    pprint( res )
+print "total number of diagnostic plots generated =", number_diagnostic_plots
