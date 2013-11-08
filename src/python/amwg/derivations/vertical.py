4db7b731b9de24d6fbbba972144299ab3213b23c
Jeff Painter <painter1@llnl.gov>
2013-11-07 16:24:56 -0800

    Don't crash on a variable without levels; just return None. name change levels to level_src because it often isn't an array of levels, it can be something else used as a source of levels

--- src/python/amwg/derivations/vertical.py
+++ src/python/amwg/derivations/vertical.py
@@ -13,13 +13,13 @@ plvlO = numpy.array([30.,50.,70.,100.,150.,200.,250.,300.,400.,500.,
 #plvlO@units = "mb"
 nplvlO = 16
 
-def verticalize( T, hyam, hybm, ps, levels=plvlO ):
+def verticalize( T, hyam, hybm, ps, level_src=plvlO ):
     """
     For data T with CAM's hybrid level coordinates, interpolates to
     the more standard pressure level coordinates and returns the results.
     The input arguments hyam, hybm, ps are the usual CAM veriables by that
     name.  Order of dimensions must be (lev,lat,lon).
-    The optional argument levels is an array or list of the new levels to which
+    The optional argument level_src is an array or list of the new level_src to which
     T should be interpolated.  Or it can be a cdms2 variable, in which case the
     levels will be obtained from its 'lev' or 'plev' axis, if any.
     """
@@ -28,15 +28,16 @@ def verticalize( T, hyam, hybm, ps, levels=plvlO ):
     p0 = 1000.   # mb
     interp = 2   # log interpolation
     extrap = False     # no extrapolation past psfc
-    if levels is None:
-        levels = plvlO
-    elif isinstance(levels,cdms2.avariable.AbstractVariable):
-        print "jfp levels=",levels,levels.id
-        lev_axis = levAxis(levels)
+    if levAxis(T) is None:
+        return None
+    if level_src is None:
+        level_src = plvlO
+    elif isinstance(level_src,cdms2.avariable.AbstractVariable):
+        lev_axis = levAxis(level_src)
         if lev_axis==None:
-            print "WARNING, no level axis in",levels.id
+            print "WARNING, no level axis in",level_src.id
             return None
-        levels = lev_axis[:]
+        level_src = lev_axis[:]
     # Convert p0 to match ps.  Later, we'll convert back to mb.  This is faster than
     # converting ps to millibars.
     if ps.units=='mb':
@@ -51,7 +52,7 @@ def verticalize( T, hyam, hybm, ps, levels=plvlO ):
     s,i = tmp.how('mbar')
     levels_orig = s*levels_orig + i
     levels_orig.units = 'mbar'
-    newT = cdutil.vertical.logLinearInterpolation( T, levels_orig, levels )
+    newT = cdutil.vertical.logLinearInterpolation( T, levels_orig, level_src )
     # Ngl doesn't work yet onoceanonly:
     #newT = Ngl.vinth2p( T, hyam, hybm, plvlO, ps, interp, p0, 1 ,extrap )
 
