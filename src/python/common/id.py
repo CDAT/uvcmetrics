cb278766d7dcd18a009acb52bb54b58c6a1c67b5
Jeff Painter <painter1@llnl.gov>
2014-04-11 15:56:35 -0700

    New class basic_id to help organize the system of assigning a unique id to objects. The id will be created in both string or tuple form.

---
+++ src/python/common/id.py
@@ -0,0 +1,17 @@
+
+
+
+class basic_id():
+    # Abstract class, mainly to document standard methods for object ids.
+    def __init__(self):
+        self._id = ()    # tuple
+        self._strid = '' # string
+    def make_id( self, *args ):
+        # Creates an id and assigns it to self._id.
+        self._id = tuple([ str(a) for a in args ])
+        self._strid = self.abbrev(self.__class__.__name__)+'_'+'_'.join(self._id)
+    def id( self ):
+        return self._id
+    abbrevs = { 'basic_filetable':'ft' }
+    def abbrev( self, str ):
+        return basic_id.abbrevs.get( str, str )
