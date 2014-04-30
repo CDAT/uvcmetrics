911c7d5a22789c52eb44a5021bae3c93486782de
Jeff Painter <painter1@llnl.gov>
2014-04-30 14:45:22 -0700

    shorter name for derived variable id, and a method adopt() to copy ids to another object

--- src/python/common/id.py
+++ src/python/common/id.py
@@ -11,6 +11,10 @@ class basic_id():
         self._strid = self.abbrev(self.__class__.__name__)+'_'+'_'.join(self._id)
     def id( self ):
         return self._id
-    abbrevs = { 'basic_filetable':'ft' }
+    abbrevs = { 'basic_filetable':'ft', 'derived_var':'var' }
     def abbrev( self, str ):
         return basic_id.abbrevs.get( str, str )
+    def adopt( self, other ):
+        "other adopts the same id as self"
+        other._id = self._id
+        other._strid = self._strid
