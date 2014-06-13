b25754cf67c47ec6dd009951c465d4f4c1b90de7
McEnerney <mcenerney1@hilbert.llnl.gov>
2014-06-13 13:13:43 -0700

    Develop plot set 9.

--- src/python/packages/amwg/amwg.py
+++ src/python/packages/amwg/amwg.py
@@ -3,6 +3,7 @@
 # Top-leve definition of AMWG Diagnostics.
 # AMWG = Atmospheric Model Working Group
 
+import pdb
 from metrics.packages.diagnostic_groups import *
 from metrics.computation.reductions import *
 from metrics.computation.plotspec import *
@@ -107,10 +108,8 @@ class amwg_plot_set4a(amwg_plot_spec):
     pass
 class amwg_plot_set7(amwg_plot_spec):
     pass
-class amwg_plot_set8(amwg_plot_spec):
-    pass
-class amwg_plot_set9(amwg_plot_spec):
-    pass
+class amwg_plot_set8(amwg_plot_spec):  
+    pass  
 class amwg_plot_set10(amwg_plot_spec):
     pass
 class amwg_plot_set11(amwg_plot_spec):
@@ -305,7 +304,6 @@ class amwg_plot_set2(amwg_plot_spec):
         psv['CAM_NCEP_HEAT_TRANSPORT_ATLANTIC'].finalize()
         psv['CAM_NCEP_HEAT_TRANSPORT_INDIAN'].finalize()
         return self.plotspec_values['CAM_NCEP_HEAT_TRANSPORT_ALL']
-
 
 class amwg_plot_set3(amwg_plot_spec,basic_id):
     """represents one plot from AMWG Diagnostics Plot Set 3.
@@ -707,17 +705,13 @@ class amwg_plot_set5and6(amwg_plot_spec):
                 v.finalize()
         return self.plotspec_values[self.plotall_id]
 
-
 class amwg_plot_set5(amwg_plot_set5and6):
     """represents one plot from AMWG Diagnostics Plot Set 5
     Each contour plot is a set of three contour plots: one each for model output, observations, and
     the difference between the two.  A plot's x-axis is longitude and its y-axis is the latitude;
-    normally a world map will be overlaid.
-    """
-    name = ' 5 - Horizontal Contour Plots of Seasonal Means'
-
-
-
+    normally a world map will be overlaid. """
+    name = ' 5- Horizontal Contour Plots of Seasonal Means'
+    
 class amwg_plot_set6(amwg_plot_set5and6):
     """represents one plot from AMWG Diagnostics Plot Set 6
     **** SO FAR, PLOT 6 VECTOR PLOTS ARE NOT DONE; ONLY Plot 5 CONTOUR ****
@@ -725,5 +719,116 @@ class amwg_plot_set6(amwg_plot_set5and6):
     the difference between the two.  A plot's x-axis is longitude and its y-axis is the latitude;
     normally a world map will be overlaid.
     """
-    name = ' 6 - Horizontal Vector Plots of Seasonal Means'
+    name = ' 6 - Horizontal Vector Plots of Seasonal Means' 
+    
+class amwg_plot_set9(amwg_plot_spec): 
+    """This class represents one plot from AMWG Diagnostics Plot Set 9.
+    Each such plot is a set of three contour plots: two for the model output and
+    the difference between the two.  A plot's x-axis is latitude and its y-axis is longitute.
+    Both model plots should have contours at the same values of their variable.  The data 
+    presented is a climatological mean - i.e., seasonal-average of the specified season, DJF, JJA, etc.
+    To generate plots use Dataset 1 in the AMWG ddiagnostics menu, set path to the directory,
+    and enter the file name.  Repeat this for dataset 2 and then apply.
+    """
+    # N.B. In plot_data.py, the plotspec contained keys identifying reduced variables.
+    # Here, the plotspec contains the variables themselves.
+    name = ' 9- Horizontal Contour Plots of DJF-JJA Differences'
+    def __init__( self, filetable1, filetable2, varid, seasonid='DJF-JJA', region=None, aux=None ):
+        """filetable1, filetable2 should be filetables for each model.
+        varid is a string, e.g. 'TREFHT'.  The seasonal difference is Seasonid
+        It is is a string, e.g. 'DJF-JJA'. """
+        import string
+        
+        #the following is for future case of setting 2 seasons
+        if "-" in seasonid:
+            _seasons = string.split(seasonid, '-')
+            if len(_seasons) == 2:
+                self._s1, self._s2 = _seasons
+        else:
+            self._s1 = 'DJF'
+            self._s2 = 'JJA'
+            seasonid = 'DJF-JJA'
 
+        plot_spec.__init__(self, seasonid)
+        self.plottype = 'Isofill'
+        self._seasonid = seasonid
+        self.season = cdutil.times.Seasons(self._seasonid)  # note that self._seasonid can differ froms seasonid
+        ft1id, ft2id = filetable_ids(filetable1, filetable2)
+
+        self.plot1_id = '_'.join([ft1id, varid, self._s1, 'contour'])
+        self.plot2_id = '_'.join([ft2id, varid, self._s2, 'contour'])
+        self.plot3_id = '_'.join([ft1id+'-'+ft2id, varid, seasonid, 'contour'])
+        self.plotall_id = '_'.join([ft1id,ft2id, varid, seasonid])
+        if not self.computation_planned:
+            self.plan_computation( filetable1, filetable2, varid, seasonid )
+    def plan_computation( self, filetable1, filetable2, varid, seasonid ):
+        self.computation_planned = False
+        #check if there is data to process
+        ft1_valid = False
+        ft2_valid = False
+        if filetable1 is not None and filetable2 is not None:
+            ft1 = filetable1.find_files(varid)
+            ft2 = filetable2.find_files(varid)
+            ft1_valid = ft1 is not None and ft1!=[]    # true iff filetable1 uses hybrid level coordinates
+            ft2_valid = ft2 is not None and ft2!=[]    # true iff filetable2 uses hybrid level coordinates
+        else:
+            print "ERROR: user must specify 2 data files"
+            return
+        if not ft1_valid or not ft2_valid:
+            return
+
+        #setup the reduced variables
+        rv_1 = reduced_variable(variableid=varid, filetable=filetable1, season=cdutil.times.Seasons(self._s1),
+                                reduction_function=reduce2latlon_seasonal) 
+        
+        rv_2 = reduced_variable(variableid=varid, filetable=filetable2, season=cdutil.times.Seasons(self._s2),
+                                reduction_function=reduce2latlon_seasonal)                                             
+                                               
+        self.reduced_variables = {rv_1.id(): rv_1, rv_2.id(): rv_2}  
+        
+        # >>>> actually last arg of the derived var should identify the coarsest level, not nec. 2
+        #generate identifiers
+        vid1 = rv.dict_id(varid, self._s1, filetable1)
+        vid2 = rv.dict_id(varid, self._s2, filetable2)
+        vid3 = dv.dict_id(varid, 'SeansonalDifference', self._seasonid, filetable1)#, ft2=filetable2)
+
+        self.derived_variables = {}
+        #create the derived variable which is the difference
+        self.derived_variables[vid3] = derived_var(vid=vid3, inputs=[vid1, vid2], func=aminusb) 
+            
+        self.single_plotspecs = {
+            self.plot1_id: plotspec(
+                vid = ps.dict_idid(vid1), 
+                zvars=[vid1], 
+                zfunc = (lambda z: z),
+                plottype = self.plottype ),
+            self.plot2_id: plotspec(
+                vid = ps.dict_idid(vid2), 
+                zvars=[vid2], 
+                zfunc = (lambda z: z),
+                plottype = self.plottype ),
+            self.plot3_id: plotspec(
+                vid = ps.dict_idid(vid3), 
+                zvars = [vid3],
+                zfunc = (lambda x: x), 
+                plottype = self.plottype )
+            }
+
+        self.composite_plotspecs = { self.plotall_id: self.single_plotspecs.keys() }
+        self.computation_planned = True
+    def _results(self, newgrid=0):
+        #pdb.set_trace()
+        results = plot_spec._results(self, newgrid)
+        if results is None:
+            print "WARNING, AMWG plot set 9 found nothing to plot"
+            return None
+        psv = self.plotspec_values
+        if self.plot1_id in psv and self.plot2_id in psv and\
+                psv[self.plot1_id] is not None and psv[self.plot2_id] is not None:
+            psv[self.plot1_id].synchronize_ranges(psv[self.plot2_id])
+        for key,val in psv.items():
+            if type(val) is not list: val=[val]
+            for v in val:
+                if v is None: continue
+                v.finalize()
+        return self.plotspec_values[self.plotall_id]
