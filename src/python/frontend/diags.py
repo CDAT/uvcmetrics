19c724a993abaafdf0c0a5245e1930bd6a149975
Jeff Painter <painter1@llnl.gov>
2014-06-19 17:52:15 -0700

    Provide for specification of two datasets in one Options object, e.g. through the command line. This uses repeated --path and --new_filter options.  We'll probably do this differently, and better, soon.

--- src/python/frontend/diags.py
+++ src/python/frontend/diags.py
@@ -1,10 +1,18 @@
 # Script for running diagnostics.
 # Usage example:
-# python src/python/frontend/diags.py --path /Users/painter1/metrics_data/cam_output --packages AMWG --output /Users/painter1/tmp/diagout --vars FLUT T
+# python src/python/frontend/diags.py --path /Users/painter1/metrics_data/cam_output --path /Users/painter1/metrics_data/obs_data --packages AMWG --output /Users/painter1/tmp/diagout --vars FLUT T
 # Or, from your Python script, call the Python function run_diagnostics(opts1,opts2).
-# Note that, if this be run from the command line, only one dataset may be supplied - but normally
-# diagnostics require two datasets.  This should be fixed in the near future, but for now the second
-# dataset is hard-coded.  To see how, search for opts2 below.
+# Normally there are two data sets.  The first will take its path from the first path argument in
+# the command line, or the path attribute (if a string; its first element if a list) in the opts1
+# argument of the Python function.
+# The second data set will take its path from the second path argument in the command line, if any.
+# In the Python function call, the path will come from a second element of the path attribute of
+# opts1 if it's a sufficiently long list; otherwise it will use the path attribute of the opts2
+# function argument.
+# Here's another command-line example with filters.  There has to be exactly as many filters as paths,
+# and this approach may change:
+# python src/python/frontend/diags.py --path /Users/painter1/metrics_data/cam_output --packages AMWG --output /Users/painter1/tmp/diagout --vars FLUT T --path ~/metrics_data/obs_data --new_filter '' --new_filter 'f_startswith("NCEP")'
+
 
 import hashlib, os, pickle, sys, os, time
 from metrics import *
@@ -30,6 +38,10 @@ def run_diagnostics( opts1, opts2=None ):
     # Each describes one data set.  The first will also be used to determine what to do with it,
     # i.e. what to plot.
 
+    #print "jfp opts1['path']=",opts1['path'],type(opts1['path'])
+    #print "jfp opts1['filter']=",opts1['filter'],type(opts1['filter'])
+    #print "jfp opts1['new_filter']=",opts1['new_filter'],type(opts1['new_filter'])
+
     outpath = opts1['output']
     if outpath is None:
         outpath = os.path.join(os.environ['HOME'],"tmp","diagout")
@@ -46,23 +58,36 @@ def run_diagnostics( opts1, opts2=None ):
 
     if type(opts1['path']) is str:
         opts1['path'] = { 1: opts1['path'] }
-    if type(opts1['path']) is list and len(opts1['path'])==1 and type(opts1['path'][0]) is str:
-        opts1['path'] = { 1: opts1['path'][0] }
+    if type(opts1['path']) is list and len(opts1['path'])>=1 and type(opts1['path'][0]) is str:
+        pathdict = {}
+        for i in range(len(opts1['path'])):
+            pathdict[i+1] = opts1['path'][i]
+        opts1['path'] = pathdict
 
+    if len(opts1['new_filter'])>0:
+        opts1['filter'] = opts1['new_filter'][0]
+    print "jfp opts1['filter']=",opts1['filter'],type(opts1['filter'])
     datafiles1 = dirtree_datafiles( opts1, 1 )
     filetable1 = datafiles1.setup_filetable()
 
-    if opts2 is None:
-        # default is obs data on my (JfP) computer.
-        rootpath = os.path.join(os.environ["HOME"],"metrics_data")
-        path2 = os.path.join(rootpath,'obs_data')
-        filt2 = f_startswith("NCEP")
-        opts2 = Options()
-        opts2._opts['path'] = {'obs':path2}
-        opts2._opts['filter'] = filt2
-        opts2._opts['cachepath']=opts1._opts['cachepath']
-    datafiles2 = dirtree_datafiles( opts2, 'obs' )
-    filetable2 = datafiles2.setup_filetable()
+    if len(opts1['path'])>1:
+        if len(opts1['new_filter'])>1:
+            opts1['filter'] = opts1['new_filter'][1]
+        print "jfp opts1['filter']=",opts1['filter'],type(opts1['filter'])
+        datafiles2 = dirtree_datafiles( opts1, 2 )
+        filetable2 = datafiles2.setup_filetable()
+    else:   # nothing in opts1 to set up the second data set
+        if opts2 is None:
+            # default is obs data on my (JfP) computer.
+            rootpath = os.path.join(os.environ["HOME"],"metrics_data")
+            path2 = os.path.join(rootpath,'obs_data')
+            filt2 = f_startswith("NCEP")
+            opts2 = Options()
+            opts2._opts['path'] = {'obs':path2}
+            opts2._opts['filter'] = filt2
+            opts2._opts['cachepath']=opts1._opts['cachepath']
+        datafiles2 = dirtree_datafiles( opts2, 'obs' )
+        filetable2 = datafiles2.setup_filetable()
 
     number_diagnostic_plots = 0
     dm = diagnostics_menu()
@@ -72,7 +97,9 @@ def run_diagnostics( opts1, opts2=None ):
         print "jfp sm=",sm
         # TO DO: more flexibility in how plot sets are identified.  And intersect requested with possible.
         if opts1['sets'] is None:
-            plotsets = [ sm.keys()[0] ]
+            keys = sm.keys()
+            keys.sort()
+            plotsets = [ keys[1] ]
         else:
             plotsets = opts1['sets']
         for sname in plotsets:
