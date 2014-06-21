28dea569a84f0a2e443108627c39190025733fc5
Jeff Painter <painter1@llnl.gov>
2014-06-20 17:31:55 -0700

    Clean up the beginning of the script - where paths and options are identified, etc. Now there are two ways to run diagnostics from a Python script - specify your files inside two Options objects, or create two filetabls and supply those instead.  Command-line usage is unchanged for the moment.

--- src/python/frontend/diags.py
+++ src/python/frontend/diags.py
@@ -1,17 +1,23 @@
 # Script for running diagnostics.
-# Usage example:
+# Command-line usage examples:
 # python src/python/frontend/diags.py --path /Users/painter1/metrics_data/cam_output --path /Users/painter1/metrics_data/obs_data --packages AMWG --output /Users/painter1/tmp/diagout --vars FLUT T
-# Or, from your Python script, call the Python function run_diagnostics(opts1,opts2).
-# Normally there are two data sets.  The first will take its path from the first path argument in
-# the command line, or the path attribute (if a string; its first element if a list) in the opts1
-# argument of the Python function.
-# The second data set will take its path from the second path argument in the command line, if any.
-# In the Python function call, the path will come from a second element of the path attribute of
-# opts1 if it's a sufficiently long list; otherwise it will use the path attribute of the opts2
-# function argument.
-# Here's another command-line example with filters.  There has to be exactly as many filters as paths,
-# and this approach may change:
+#
 # python src/python/frontend/diags.py --path /Users/painter1/metrics_data/cam_output --packages AMWG --output /Users/painter1/tmp/diagout --vars FLUT T --path ~/metrics_data/obs_data --new_filter '' --new_filter 'f_startswith("NCEP")'
+
+# Python usage example:
+# opts = Options()
+# opts['cachepath'] = '/tmp'
+# opts['outpath'] = '~/tmp/diagout'
+# opts['packages'] = ['AMWG']
+# opts['sets'] = [' 3 - Line Plots of  Zonal Means']
+# opts['vars'] = ['T','FLUT']
+# filetable1 = path2filetable( opts, path='~/metrics_data/cam_output')
+# filetable2 = path2filetable( opts, path='~/metrics_data/obs_data', filter='f_startswith("NCEP")')
+# run_diagnostics_from_filetables( opts, filetable1, filetable2 )
+#
+# You also could create a more elaborate Options object, or two of them, in which case you won't
+# have to explicitly create the filetables.
+
 
 
 import hashlib, os, pickle, sys, os, time
@@ -33,61 +39,69 @@ def mysort( lis ):
     lis.sort()
     return lis
 
-def run_diagnostics( opts1, opts2=None ):
+def run_diagnostics_from_options( opts1, opts2=None ):
     # Input is one or two instances of Options, normally two.
     # Each describes one data set.  The first will also be used to determine what to do with it,
     # i.e. what to plot.
 
-    #print "jfp opts1['path']=",opts1['path'],type(opts1['path'])
-    #print "jfp opts1['filter']=",opts1['filter'],type(opts1['filter'])
-    #print "jfp opts1['new_filter']=",opts1['new_filter'],type(opts1['new_filter'])
-
-    outpath = opts1['output']
-    if outpath is None:
-        outpath = os.path.join(os.environ['HOME'],"tmp","diagout")
-    if opts1['packages'] is None:
-        packages = ['AMWG']
-    else:
-        packages = opts1['packages']
-    if opts1.get( 'seasons', None ) is None:
-        seasons = ['ANN']
-    else:
-        seasons = opts1['seasons']
-    if opts1['varopts'] is None:
-        opts1['varopts'] = [None]
+    path1 = None
+    path2 = None
+    filt1 = None
+    filt2 = None
 
     if type(opts1['path']) is str:
-        opts1['path'] = { 1: opts1['path'] }
-    if type(opts1['path']) is list and len(opts1['path'])>=1 and type(opts1['path'][0]) is str:
+        path1 = opts1['path']
+    if type(opts1['path']) is list and type(opts1['path'][0]) is str:
         pathdict = {}
         for i in range(len(opts1['path'])):
             pathdict[i+1] = opts1['path'][i]
-        opts1['path'] = pathdict
-
+        path1 = pathdict[1]
+        if 2 in pathdict:
+            path2 = pathdict[2]
     if len(opts1['new_filter'])>0:
-        opts1['filter'] = opts1['new_filter'][0]
-    print "jfp opts1['filter']=",opts1['filter'],type(opts1['filter'])
-    datafiles1 = dirtree_datafiles( opts1, 1 )
-    filetable1 = datafiles1.setup_filetable()
+        filt1 = opts1['new_filter'][0]
+ 
+    print "jfp path1=",path1,"filt1=",filt1,"X"
+    filetable1 = path2filetable( opts1, path=path1, filter=filt1 )
 
-    if len(opts1['path'])>1:
-        if len(opts1['new_filter'])>1:
-            opts1['filter'] = opts1['new_filter'][1]
-        print "jfp opts1['filter']=",opts1['filter'],type(opts1['filter'])
-        datafiles2 = dirtree_datafiles( opts1, 2 )
-        filetable2 = datafiles2.setup_filetable()
-    else:   # nothing in opts1 to set up the second data set
+    if path2 is None:
         if opts2 is None:
             # default is obs data on my (JfP) computer.
             rootpath = os.path.join(os.environ["HOME"],"metrics_data")
             path2 = os.path.join(rootpath,'obs_data')
             filt2 = f_startswith("NCEP")
-            opts2 = Options()
-            opts2._opts['path'] = {'obs':path2}
-            opts2._opts['filter'] = filt2
-            opts2._opts['cachepath']=opts1._opts['cachepath']
-        datafiles2 = dirtree_datafiles( opts2, 'obs' )
-        filetable2 = datafiles2.setup_filetable()
+        else:
+            # If opts2 were created, there would be no point in putting a list of paths there.
+            # So keep it simple:
+            path2 = opts2['path']
+            filt2 = opts2['filter']
+    else:
+        if len(opts1['new_filter'])>1:
+            filt2 = opts1['new_filter'][1]
+
+    print "jfp path2=",path2,"filt2=",filt2,"X"
+    filetable2 = path2filetable( opts1, path=path2, filter=filt2 )
+
+    run_diagnostics_from_filetables( opts1, filetable1, filetable2 )
+
+def run_diagnostics_from_filetables( opts, filetable1, filetable2=None ):
+    """Runs the diagnostics.  The data is specified by the filetables.
+    Most other choices, such as the plot sets, variables, and seasons, are specified in opts,
+    an instance of Options."""
+
+    outpath = opts['output']
+    if outpath is None:
+        outpath = os.path.join(os.environ['HOME'],"tmp","diagout")
+    if opts['packages'] is None:
+        packages = ['AMWG']
+    else:
+        packages = opts['packages']
+    if opts.get( 'seasons', None ) is None:
+        seasons = ['ANN']
+    else:
+        seasons = opts['seasons']
+    if opts['varopts'] is None:
+        opts['varopts'] = [None]
 
     number_diagnostic_plots = 0
     dm = diagnostics_menu()
@@ -96,12 +110,12 @@ def run_diagnostics( opts1, opts2=None ):
         sm = pclass.list_diagnostic_sets()
         print "jfp sm=",sm
         # TO DO: more flexibility in how plot sets are identified.  And intersect requested with possible.
-        if opts1['sets'] is None:
+        if opts['sets'] is None:
             keys = sm.keys()
             keys.sort()
             plotsets = [ keys[1] ]
         else:
-            plotsets = opts1['sets']
+            plotsets = opts['sets']
         for sname in plotsets:
             sclass = sm[sname]
             print "jfp sclass.name=",sclass.name
@@ -109,11 +123,11 @@ def run_diagnostics( opts1, opts2=None ):
             for seasonid in seasons:
                 print "jfp seasonid=",seasonid
                 variables = pclass.list_variables( filetable1, filetable2, sname  )
-                if opts1.get('vars',['ALL'])!=['ALL']:
-                    print "jfp opts1 vars=",opts1['vars']
-                    variables = list( set(variables) & set(opts1.get('vars',[])) )
-                    if len(variables)==0 and len(opts1.get('vars',[]))>0:
-                        print "WARNING: Couldn't find any of the requested variables:",opts1['vars']
+                if opts.get('vars',['ALL'])!=['ALL']:
+                    print "jfp opts vars=",opts['vars']
+                    variables = list( set(variables) & set(opts.get('vars',[])) )
+                    if len(variables)==0 and len(opts.get('vars',[]))>0:
+                        print "WARNING: Couldn't find any of the requested variables:",opts['vars']
                 for varid in variables:
                     print "jfp varid=",varid
                     vard = pclass.all_variables( filetable1, filetable2, sname )
@@ -121,7 +135,7 @@ def run_diagnostics( opts1, opts2=None ):
                     varopts = var.varoptions()
                     if varopts is None:
                         varopts = [None]
-                    varopts = list( set(varopts) & set(opts1['varopts']) )
+                    varopts = list( set(varopts) & set(opts['varopts']) )
                     for aux in varopts:
                         plot = sclass( filetable1, filetable2, varid, seasonid, aux )
                         res = plot.compute(newgrid=-1) # newgrid=0 for original grid, -1 for coarse
@@ -140,4 +154,4 @@ if __name__ == '__main__':
    o = Options()
    o.processCmdLine()
    o.verifyOptions()
-   run_diagnostics(o)
+   run_diagnostics_from_options(o)
