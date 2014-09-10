4d7a4b55041249b39241919df3734827b46c2d07
Jeff Painter <painter1@llnl.gov>
2014-09-10 14:55:43 -0700

    New function, to convert season from a string to a cdutil.times.Seasons object.

--- src/python/common/utilities.py
+++ src/python/common/utilities.py
@@ -5,3 +5,17 @@ def natural_sort(l):
     convert = lambda text: int(text) if text.isdigit() else text.lower() 
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
     return sorted(l, key = alphanum_key)
+
+def season2Season(season):
+    """This function helps make foolproof the season argument of other functions.
+    If it is a string or None, it converts it to a cdutil.times.Seasons object and returns it.
+    Otherwise, it is just returned.
+    """
+    if type(season) is str or season is None:
+        seasonid = season  # don't have to do this, but it still feels safer
+        if seasonid=='ANN' or seasonid is None or seasonid=='':
+            # cdutil.times doesn't recognize 'ANN'
+            seasonid='JFMAMJJASOND'
+        return cdutil.times.Seasons(seasonid)
+    else:
+        return season
