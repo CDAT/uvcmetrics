48df4a1f45bf94026288dfe930c65d77535b9e77
Jeff Painter <painter1@llnl.gov>
2014-10-07 11:53:22 -0700

    Add tests for newly implemented diagnostics.  We now can do all 10 Tier 1a plots.

--- src/python/frontend/tier1a.sh
+++ src/python/frontend/tier1a.sh
@@ -5,8 +5,10 @@ diags --path ~/metrics_data/cam35_data --packages AMWG --outputdir ~/tmp/diagout
 diags --path ~/metrics_data/cam35_data --packages AMWG --outputdir ~/tmp/diagout --path2 /Users/painter1/metrics_data/obs_data --filter2 "f_startswith('CERES-EBAF')" --seasons ANN --sets 5 --vars SWCF
 # 3. LW Cloud Forcing, LWCF
 diags --path ~/metrics_data/cam35_data --packages AMWG --outputdir ~/tmp/diagout --path2 /Users/painter1/metrics_data/obs_data --filter2 "f_startswith('CERES-EBAF')" --seasons ANN --sets 5 --vars LWCF
-# 4 Global Precipitation, PRECT - not done, will be done next
-# 5 Land 2-m temperature, TREFHT - not done, will be done second
+# 4 Global Precipitation, PRECT
+diags --path ~/metrics_data/cam35_data --packages AMWG --outputdir ~/tmp/diagout --path2 /Users/painter1/metrics_data/obs_data --filter2 "f_startswith('GPCP')" --seasons ANN --sets 5 --vars PRECT
+# 5 Land 2-m temperature, TREFHT
+diags --path ~/metrics_data/cam35_data --packages AMWG --outputdir ~/tmp/diagout --path2 /Users/painter1/metrics_data/obs_data --filter2 "f_startswith('WILLMOTT')" --seasons ANN --sets 5 --vars TREFHT
 # 6 Oceanic Surface Wind Stress, STRESS
 diags --path ~/metrics_data/cam35_data/ --path2 /Users/painter1/metrics_data/obs_data_5.6/  --filter2 "f_startswith('ERS')" --package AMWG --set 6 --var STRESS --seasons ANN
 # 7. 300 mb Zonal Wind, U
@@ -16,7 +18,7 @@ diags --path ~/metrics_data/cam35_data --packages AMWG --outputdir ~/tmp/diagout
 # 9. Zonal Mean Temperature, T
 diags --path ~/metrics_data/cam35_data --packages AMWG --outputdir ~/tmp/diagout --path2 /Users/painter1/metrics_data/obs_data --filter2 "f_startswith('ERAI')" --seasons ANN --sets 4 --vars T
 # 10. Aerosol Optical Depth - semi-done, don't have a suitable data file to test on
-diags --path ~/metrics_data/cam35_data --packages AMWG --outputdir ~/tmp/diagout --path2 /Users/painter1/metrics_data/obs_data --filter2 "f_startswith('MISR')" --seasons ANN --sets 5 --vars AODVIS
+diags --path ~/metrics_data/acme_data --packages AMWG --outputdir ~/tmp/diagout --path2 /Users/painter1/metrics_data/acme_obs --filter2 "f_startswith('sat')" --seasons ANN --sets 5 --vars AODVIS
  
 
  
