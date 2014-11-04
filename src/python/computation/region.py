a944dc4fd626b88bfc5b53fad586a8b912395ef2
Jeff Painter <painter1@llnl.gov>
2014-11-04 12:01:49 -0800

    New file, has a new class rectregion for a rectangular lat-lon region. This can be used just the same as the list [latmin,latmax,lonmin,lonmax] but it also carries along a name for the region.

---
+++ src/python/computation/region.py
@@ -0,0 +1,19 @@
+from metrics.common.id import *
+
+class rectregion(basic_id):
+    """represents a rectangular (in lat-lon coordinates) region.
+    It has a name and min,max values for lat,lon."""
+    def __init__( self, name, latlonminmax ):
+        """name is a string naming the region, e.g. "Greenland".
+        latlonminmax is a list, [latmin,latmax,lonmin,lonmax], e.g. [60, 90, -60, -20]
+        """
+        self._name = name
+        self.latlonminmax = latlonminmax
+        basic_id.__init__( self, self.__class__.__name__, name )
+    def __getitem__( self, slice ):
+        """Returns lat/lon min/max from the list of region lat-lon bounds."""
+        return self.latlonminmax.__getitem__(slice)
+    def __str__( self ):
+        return self._name
+    def __repr__( self ):
+        return id2str( self.id() )
