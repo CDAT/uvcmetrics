d8dbfbc4317ab21838298427631438e4da842b87
Jeff Painter <painter1@llnl.gov>
2015-02-19 16:57:50 -0800

    Update diags.py options to match recent improvements to options.py.

--- src/python/frontend/lotsa_diags.sh
+++ src/python/frontend/lotsa_diags.sh
@@ -1,38 +1,38 @@
 # lots of "diags" runs, for testing
 echo "Atmosphere"
 echo "set 1"
-diags --path ~/metrics_data/cam35_data/ --path2 /Users/painter1/metrics_data/obs_data_5.6/  --package AMWG --set 1 --seasons ANN
+diags --outputdir ~/tmp/diagout/ --model path=~/metrics_data/cam35_data/,climos=yes --obs path=/Users/painter1/metrics_data/obs_data_5.6/,climos=yes  --package AMWG --set 1 --seasons ANN
 echo "set 2"
-diags --path /Users/painter1/metrics_data/cam35_data/ --package AMWG --set 2 --var Ocean_Heat --seasons ANN --path2 ~/metrics_data/obs_data_5.6/ --filter2 "f_startswith('NCEP')"
+diags --outputdir ~/tmp/diagout/ --model path=/Users/painter1/metrics_data/cam35_data/,climos=yes --obs path=~/metrics_data/obs_data_5.6/,filter="f_startswith('NCEP')",climos=yes --package AMWG --set 2 --var Ocean_Heat --seasons ANN
 echo "set 3"
-diags --path ~/metrics_data/cam35_data/ --path2 /Users/painter1/metrics_data/obs_data_5.6/  --filter2 "f_startswith('JRA25')" --package AMWG --set 3 --var TREFHT --seasons ANN
+diags --outputdir ~/tmp/diagout/ --model path=~/metrics_data/cam35_data/,climos=yes --obs path=/Users/painter1/metrics_data/obs_data_5.6/,filter="f_startswith('JRA25')",climos=yes --package AMWG --set 3 --var TREFHT --seasons ANN
 echo "set 4"
-diags --path ~/metrics_data/cam35_data/ --path2 /Users/painter1/metrics_data/obs_data_5.6/  --filter2 "f_startswith('NCEP')" --package AMWG --set 4 --vars T --seasons ANN
+diags --outputdir ~/tmp/diagout/ --model path=~/metrics_data/cam35_data/,climos=yes --obs path=/Users/painter1/metrics_data/obs_data_5.6/,filter="f_startswith('NCEP')",climos=yes --package AMWG --set 4 --vars T --seasons ANN
 echo "set 5"
-diags --path /Users/painter1/metrics_data/cam35_data/ --package AMWG --set 5 --var T --seasons ANN --path2 ~/metrics_data/obs_data_5.6/ --filter2 "f_startswith('NCEP')" --varopts 850
+diags --outputdir ~/tmp/diagout/ --model path=/Users/painter1/metrics_data/cam35_data/,climos=yes --obs path=~/metrics_data/obs_data_5.6/,filter="f_startswith('NCEP')",climos=yes --varopts 850 --package AMWG --set 5 --var T --seasons ANN
 echo "set 6"
-diags --path ~/metrics_data/cam35_data/ --path2 /Users/painter1/metrics_data/obs_data_5.6/  --filter2 "f_startswith('ERS')" --package AMWG --set 6 --var STRESS --seasons ANN
+diags --outputdir ~/tmp/diagout/ --model path=~/metrics_data/cam35_data/,climos=yes --obs path=/Users/painter1/metrics_data/obs_data_5.6/,filter="f_startswith('ERS')",climos=yes --package AMWG --set 6 --var STRESS --seasons ANN
 echo "set 7"
-diags --path ~/metrics_data/cam35_data/ --path2 /Users/painter1/metrics_data/obs_data_5.6/  --filter2 "f_startswith('NCEP')" --package AMWG --set 7 --seasons ANN --vars T --varopts ' Northern Hemisphere'
+diags --outputdir ~/tmp/diagout/ --model path=~/metrics_data/cam35_data/,climos=yes --obs path=/Users/painter1/metrics_data/obs_data_5.6/,filter="f_startswith('NCEP')",climos=yes --package AMWG --set 7 --seasons ANN --vars T --varopts ' Northern Hemisphere'
 echo "set 8"
-diags --path ~/metrics_data/cam35_data/ --path2 /Users/painter1/metrics_data/obs_data_5.6/  --filter2 "f_startswith('NCEP')" --package AMWG --set 8 --seasons ANN --vars T
+diags --outputdir ~/tmp/diagout/ --model path=~/metrics_data/cam35_data/,climos=yes --obs path=/Users/painter1/metrics_data/obs_data_5.6/,filter="f_startswith('NCEP')",climos=yes --package AMWG --set 8 --seasons ANN --vars T
 echo "set 9"
-diags --path ~/metrics_data/cam35_data/ --path2 /Users/painter1/metrics_data/obs_data_5.6/  --filter2 "f_startswith('NCEP')" --package AMWG --set 9 --seasons ANN --vars T
+diags --outputdir ~/tmp/diagout/ --model path=~/metrics_data/cam35_data/,climos=yes --obs path=/Users/painter1/metrics_data/obs_data_5.6/,filter="f_startswith('NCEP')",climos=yes --package AMWG --set 9 --seasons ANN --vars T
 echo "set 10"
-diags --path ~/metrics_data/cam35_data/ --path2 /Users/painter1/metrics_data/obs_data_5.6/  --filter2 "f_startswith('NCEP')" --package AMWG --set 10 --seasons ANN --vars TS
+diags --outputdir ~/tmp/diagout/ --model path=~/metrics_data/cam35_data/,climos=yes --obs path=/Users/painter1/metrics_data/obs_data_5.6/,filter="f_startswith('NCEP')",climos=yes --package AMWG --set 10 --seasons ANN --vars TS
 echo "set 11"
-diags --path ~/metrics_data/cam35_data/ --path2 /Users/painter1/metrics_data/obs_data_5.6/  --filter2 "f_startswith('NCEP')" --package AMWG --set 11 --seasons ANN --vars TS
+diags --outputdir ~/tmp/diagout/ --model path=~/metrics_data/cam35_data/,climos=yes --obs path=/Users/painter1/metrics_data/obs_data_5.6/,filter="f_startswith('NCEP')",climos=yes --package AMWG --set 11 --seasons ANN --vars TS
 echo "set 13"
 mkdir ~/tmp/diagout/13
-diags --packages AMWG --outputdir ~/tmp/diagout/13 --path ~/metrics_data/cam35_data/  --path2 ~/metrics_data/obs_data_5.6/ --filter2 'f_startswith("ISCCPCOSP")' --seasons ANN --set 13 --vars CLISCCP
+diags --outputdir ~/tmp/diagout/13 --model path=~/metrics_data/cam35_data/,climos=yes --obs path=~/metrics_data/obs_data_5.6/,filter='f_startswith("ISCCPCOSP")' --package AMWG --set 13 --seasons ANN --vars CLISCCP
 # Note that the next test uses a special dataset.  At PCMDI you can get it by
 #  sudo mount saigon2:/A-Train_data /A-Train_data
 echo "cloud variables in set 5"
 mkdir ~/tmp/diagout/cosp
-diags --packages AMWG --outputdir ~/tmp/diagout/cosp --path /A-Train_data/integration_cosp_cam/amip10yr --filter "f_startswith('cam5_2deg_release_amip.cam2.h0.2005')" --path2 ~/metrics_data/obs_data_5.6 --filter2 "f_startswith('CALIPSOCOSP')" --seasons ANN --sets 5 --vars CLDTOT_CAL CLDHGH_CAL
-diags --packages AMWG --outputdir ~/tmp/diagout/cosp --path /A-Train_data/integration_cosp_cam/amip10yr --filter "f_startswith('cam5_2deg')" --path2 ~/metrics_data/obs_data_5.6 --filter2 "f_startswith('MODISCOSP')" --seasons ANN --sets  5 --vars CLDHGH_TAU1.3_MODIS
+diags --outputdir ~/tmp/diagout/cosp --model path=/A-Train_data/integration_cosp_cam/amip10yr,filter="f_startswith('cam5_2deg_release_amip.cam2.h0.2005')",climos=no --obs path=~/metrics_data/obs_data_5.6/,filter="f_startswith('CALIPSOCOSP')",climos=yes --package AMWG --sets 5 --seasons ANN --vars CLDTOT_CAL CLDHGH_CAL
+diags --outputdir ~/tmp/diagout/cosp --model path=/A-Train_data/integration_cosp_cam/amip10yr,filter="f_startswith('cam5_2deg')",climos=no --obs path=~/metrics_data/obs_data_5.6/,filter="f_startswith('MODISCOSP')",climos=yes --package AMWG --sets  5 --seasons ANN --vars CLDHGH_TAU1.3_MODIS
 
 echo "Land.  I don't have suitable obs data."
-diags --path ~/metrics_data/smaller_testdata/acme_hires_land --package LMWG --set 1 --vars RAIN
+diags --outputdir ~/tmp/diagout/ --model path=~/metrics_data/smaller_testdata/acme_hires_land,climos=no --package LMWG --set 1 --vars RAIN
 echo "set 2, only does model data for LHEAT"
-diags --path ~/metrics_data/acme_clm_climo --package LMWG --set 2 --vars LHEAT
+diags --outputdir ~/tmp/diagout/ --model path=~/metrics_data/acme_clm_climo,climos=yes --package LMWG --set 2 --vars LHEAT
--- src/python/frontend/tier1a.sh
+++ src/python/frontend/tier1a.sh
@@ -3,45 +3,45 @@
 # 1. Sea Level Pressure, PSL
 echo '1. Sea Level Pressure, PSL'
 mkdir ~/tmp/diagout/1
-diags --packages AMWG --outputdir ~/tmp/diagout/1 --path ~/metrics_data/cam35_data --path2 ~/metrics_data/obs_data_5.6 --filter2 "f_startswith('ERAI')" --seasons ANN --sets 5 --vars PSL
+diags --package AMWG --outputdir ~/tmp/diagout/1 --model path=~/metrics_data/cam35_data,climos=yes --obs path=~/metrics_data/obs_data_5.6,filter="f_startswith('ERAI')",climos=yes --seasons ANN --sets 5 --vars PSL
 # 2. SW Cloud Forcing, SWCF
 echo '2. SW Cloud Forcing, SWCF'
 mkdir ~/tmp/diagout/2
-diags --packages AMWG --outputdir ~/tmp/diagout/2 --path ~/metrics_data/cam35_data --path2 ~/metrics_data/obs_data_5.6 --filter2 "f_startswith('CERES-EBAF')" --seasons ANN --sets 5 --vars SWCF
+diags --package AMWG --outputdir ~/tmp/diagout/2 --model path=~/metrics_data/cam35_data,climos=yes --obs path=~/metrics_data/obs_data_5.6,filter="f_startswith('CERES-EBAF')",climos=yes --seasons ANN --sets 5 --vars SWCF
 # 3. LW Cloud Forcing, LWCF
 echo '3. LW Cloud Forcing, LWCF'
 mkdir ~/tmp/diagout/3
-diags --packages AMWG --outputdir ~/tmp/diagout/3 --path ~/metrics_data/cam35_data --path2 ~/metrics_data/obs_data_5.6 --filter2 "f_startswith('CERES-EBAF')" --seasons ANN --sets 5 --vars LWCF
+diags --package AMWG --outputdir ~/tmp/diagout/3 --model path=~/metrics_data/cam35_data,climos=yes --obs path=~/metrics_data/obs_data_5.6,filter="f_startswith('CERES-EBAF')",climos=yes --seasons ANN --sets 5 --vars LWCF
 # 4 Global Precipitation, PRECT
 echo '4 Global Precipitation, PRECT'
 mkdir ~/tmp/diagout/4
-diags --packages AMWG --outputdir ~/tmp/diagout/4 --path ~/metrics_data/cam35_data --path2 ~/metrics_data/obs_data_5.6 --filter2 "f_startswith('GPCP')" --seasons ANN --sets 5 --vars PRECT
+diags --package AMWG --outputdir ~/tmp/diagout/4 --model path=~/metrics_data/cam35_data,climos=yes --obs path=~/metrics_data/obs_data_5.6,filter="f_startswith('GPCP')",climos=yes --seasons ANN --sets 5 --vars PRECT
 # 5 Land 2-m temperature, TREFHT
 echo '5 Land 2-m temperature, TREFHT'
 mkdir ~/tmp/diagout/5
-diags --packages AMWG --outputdir ~/tmp/diagout/5 --path ~/metrics_data/cam35_data --path2 ~/metrics_data/obs_data_5.6 --filter2 "f_startswith('WILLMOTT')" --seasons ANN --sets 5 --vars TREFHT
+diags --package AMWG --outputdir ~/tmp/diagout/5 --model path=~/metrics_data/cam35_data,climos=yes --obs path=~/metrics_data/obs_data_5.6,filter="f_startswith('WILLMOTT')",climos=yes --seasons ANN --sets 5 --vars TREFHT
 # 6 Oceanic Surface Wind Stress, STRESS
 echo '6 Oceanic Surface Wind Stress, STRESS'
 mkdir ~/tmp/diagout/6
-diags --packages AMWG --outputdir ~/tmp/diagout/6 --path ~/metrics_data/cam35_data/ --path2 ~/metrics_data/obs_data_5.6 --filter2 "f_startswith('ERS')" --set 6 --var STRESS --seasons ANN
+diags --package AMWG --outputdir ~/tmp/diagout/6 --model path=~/metrics_data/cam35_data,climos=yes/ --obs path=~/metrics_data/obs_data_5.6,filter="f_startswith('ERS')",climos=yes --seasons ANN --set 6 --var STRESS
 # 7. 300 mb Zonal Wind, U
 echo '7. 300 mb Zonal Wind, U'
 mkdir ~/tmp/diagout/7
-diags --packages AMWG --outputdir ~/tmp/diagout/7 --path ~/metrics_data/cam35_data --path2 ~/metrics_data/obs_data_5.6 --filter2 "f_startswith('ERAI')" --seasons ANN --sets 5 --vars U --varopts 300
+diags --package AMWG --outputdir ~/tmp/diagout/7 --model path=~/metrics_data/cam35_data,climos=yes --obs path=~/metrics_data/obs_data_5.6,filter="f_startswith('ERAI')",climos=yes --seasons ANN --sets 5 --vars U --varopts 300
 # 8. Zonal Mean Relative Humidity, RELHUM
 echo '8. Zonal Mean Relative Humidity, RELHUM'
 mkdir ~/tmp/diagout/8
-diags --packages AMWG --outputdir ~/tmp/diagout/8 --path ~/metrics_data/cam35_data --path2 ~/metrics_data/obs_data_5.6 --filter2 "f_startswith('ERAI')" --seasons ANN --sets 4 --vars RELHUM
+diags --package AMWG --outputdir ~/tmp/diagout/8 --model path=~/metrics_data/cam35_data,climos=yes --obs path=~/metrics_data/obs_data_5.6,filter="f_startswith('ERAI')",climos=yes --seasons ANN --sets 4 --vars RELHUM
 # 9. Zonal Mean Temperature, T
 echo '9. Zonal Mean Temperature, T'
 mkdir ~/tmp/diagout/9
-diags --packages AMWG --outputdir ~/tmp/diagout/9 --path ~/metrics_data/cam35_data --path2 ~/metrics_data/obs_data_5.6 --filter2 "f_startswith('ERAI')" --seasons ANN --sets 4 --vars T
+diags --package AMWG --outputdir ~/tmp/diagout/9 --model path=~/metrics_data/cam35_data,climos=yes --obs path=~/metrics_data/obs_data_5.6,filter="f_startswith('ERAI')",climos=yes --seasons ANN --sets 4 --vars T
 # 10. Aerosol Optical Depth, AODVIS
 echo '10. Aerosol Optical Depth, AODVIS'
 mkdir ~/tmp/diagout/10
-  diags --packages AMWG --outputdir ~/tmp/diagout/10 --path ~/metrics_data/acme_data/b1850c5_t2 --path2 ~/metrics_data/acme_obs --filter2 "f_startswith('sat')" --seasons ANN --sets 5 --vars AODVIS
+  diags --package AMWG --outputdir ~/tmp/diagout/10 --model path=~/metrics_data/acme_data/b1850c5_t2 --obs path=~/metrics_data/acme_obs,filter="f_startswith('sat')",climos=yes --seasons ANN --sets 5 --vars AODVIS
 
 # Scalars are in the "plot set 1" table:
 echo 'Scalars are in the "plot set 1" table'
 mkdir ~/tmp/diagout/table
-diags --packages AMWG --outputdir ~/tmp/diagout --path ~/metrics_data/cam_output --path2 ~/metrics_data/obs_data_5.6 --seasons ANN --sets 1
+diags --package AMWG --outputdir ~/tmp/diagout --model path=~/metrics_data/cam_output,climos=no --obs path=~/metrics_data/obs_data_5.6 --seasons ANN --sets 1
