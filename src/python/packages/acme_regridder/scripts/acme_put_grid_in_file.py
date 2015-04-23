e565a441a6c328a344d3ab4e48d4ad0a3c13ecdf
Charles Doutriaux <doutriaux1@llnl.gov>
2015-04-22 17:09:02 -0700

    first pass add script to add grid info in file

---
+++ src/python/packages/acme_regridder/scripts/acme_put_grid_in_file.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# PUts grid file info in file
+import cdms2 
+import sys, os
+import numpy
+import MV2
+import argparse
+
+if __name__=="__main__":
+  value = 0
+  cdms2.setNetcdfShuffleFlag(value) ## where value is either 0 or 1
+  cdms2.setNetcdfDeflateFlag(value) ## where value is either 0 or 1
+  cdms2.setNetcdfDeflateLevelFlag(value) ## where value is a integer between 0 and 9 included
+
+  ## Create the parser for user input
+  parser = argparse.ArgumentParser(description='Given an output file and a grid file, extract select (or all) variables from the output file and adds the grid file inof necessary for cdms2/CF read')
+  parser.add_argument("--grid-file","-g",dest="grid",help="path to grid file",required=True)
+  parser.add_argument("--output","-o",dest="out",help="output file")
+  parser.add_argument("--input","-i","-f","--file",dest="file",help="input file to process",required=True)
+  parser.add_argument("--var","-v",dest="vars",default=[],
+      action="append",help="variable to process (default is all variable with 'ncol' dimension")
+
+  args = parser.parse_args(sys.argv[1:])
+  
+  ## First we will open the input file
+  f=cdms2.open(args.file)
+
+  ## Now quick check that vars asked for are in
+  for v in args.vars:
+    if not v in f.variables.keys():
+      raise RuntimeError("Requested variable: %s is not in input file: %s" % (v,args.file))
+
+  vars = args.vars
+  if vars == []:
+    vars=f.variables.keys()
+  if not "lat" in vars:
+    vars.append("lat")
+  if not "lon" in vars:
+    vars.append("lon")
+
+  ## Now we will open the grid file
+  fg=cdms2.open(args.grid)
+
+  ## Preparing output file
+  if args.out is None:
+    args.out = args.file[:-3]+"_with_grid_info.nc"
+
+  fo=cdms2.open(args.out,"w")
+  for v in vars:
+    print "Dealing with:",v
+    try:
+      V=f(v)
+      try:
+        axIds = V.getAxisIds()
+      except:
+        axIds = [] # num variable
+      if v=="lat":
+        if hasattr(V,"bounds"):
+          print "LAT OBNUDS:",V.bounds
+        else:
+          V.bounds="grid_corner_lat"
+        ## Ok now we need to read in the vertices
+        b = fg(V.bounds)
+        b.setAxis(0,V.getAxis(-1))
+        fo.write(b)
+      elif v=="lon":
+        if hasattr(V,"bounds"):
+          print "LON OBNUDS:",V.bounds
+        else:
+          V.bounds="grid_corner_lon"
+        ## Ok now we need to read in the vertices
+        b = fg(V.bounds)
+        b.setAxis(0,V.getAxis(-1))
+        fo.write(b)
+      if "ncol" in axIds:
+        print "tweaking %s" % v
+        if hasattr(V,"coordinates"):
+          print V.coordinates
+        else:
+          V.coordinates="ncol"
+      else:
+        print "Storing %s as is" % v
+      fo.write(V)
+    except Exception,err:
+      print "Error processing: %s\n%s" % (v,err)
+fo.close()
+print "Done, new file is: %s" % args.out
