17eb1bc6791f51726362b7edf2ed5b564d18ef4d
Jeff Painter <painter1@llnl.gov>
2015-06-23 09:04:00 -0700

    make it possible to import acme_regridder/scripts/acme_regrid.py

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ packages = {'metrics': 'src/python',
               'metrics.packages.amwg.derivations': 'src/python/packages/amwg/derivations',
               'metrics.packages.lmwg': 'src/python/packages/lmwg',
               'metrics.packages.acme_regridder': 'src/python/packages/acme_regridder',
+              'metrics.packages.acme_regridder.scripts': 'src/python/packages/acme_regridder/scripts',
             }
 for d in packages.itervalues():
     shutil.copy("git.py",os.path.join(d,"git.py"))
@@ -50,6 +51,7 @@ setup (name = "metrics",
                    'metrics.common',
                    'metrics.packages.wgne',
                    'metrics.packages.acme_regridder',
+                   'metrics.packages.acme_regridder.scripts',
                    'metrics.packages.amwg',
                    'metrics.packages.amwg.derivations',
                    'metrics.packages.lmwg',
