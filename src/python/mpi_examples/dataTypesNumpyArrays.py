c23cf1b494685cc510de4374c1b4ca19329bb802
mcenerney1 <mcenerney1@mcenerney1ml.llnl.gov>
2015-08-26 08:14:40 -0700

    The example in mpi_example that is important to look at is dataTypesNumpyArrays. It illustrates how to simulate a dictionary with numpy arrays that can be gathered.

--- src/python/mpi_examples/dataTypesNumpyArrays.py
+++ src/python/mpi_examples/dataTypesNumpyArrays.py
@@ -1,4 +1,7 @@
-""" This app trys mpi data type with numpy arrays. """
+""" mpi4py does not handle gathering dictionaries where the values are numpy arrays
+What is required is to define a numpy type that simulates the key-value pair of a dictionary.
+It is required to setup an MPI struct that matches this data type.  All of this must be done
+befor the Gather step.  This app illustrates what has to be done. """
 
 from mpi4py import MPI
 import numpy as np
@@ -6,36 +9,46 @@ import numpy as np
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-npydt = np.dtype([('name', np.str_, 4), ('grades', np.float16, (2,))])
-print rank, npydt
-x = np.array([('Sara', (8.0, 7.0)), ('John', (6.0, 7.0))], dtype=npydt)
+#define memory allocation parameters
+nstr = 8
+nfloat = 2
+
+#define custom numpy data type
+npdt = np.dtype([('name', np.str_, nstr), ('grades', np.float, (nfloat,))])
+print rank, npdt
+x = np.array([('Sarah12345', (8.0, 7.0)), ('John', (6.0, 5.0))], dtype=npdt)
+print x
 #print x['name']
 #print x['grades']
 #print type(x['grades'])
-
-nStringNbytes = x['name'][0].nbytes #16
-nArrayNbytes = x['grades'][0].nbytes
-print rank, nStringNbytes, nArrayNbytes
 comm.barrier()
+
+#define mpi struct that matches the numpy type
 struct = MPI.Datatype.Create_struct( # MPI user-defined datatype
-    [nStringNbytes, nArrayNbytes], # block lengths
-    [0, nStringNbytes], # displacements in bytes
+    [nstr, nfloat], # block lengths
+    [0, nstr], # displacements in bytes
     [MPI.CHAR,  MPI.DOUBLE], # MPI datatypes
 )
-
 mpidt = struct.Commit() # don't forget to call Commit() after creation !!!
 print rank, mpidt
 
+#scatter the keys
 name = comm.scatter(x['name'], root=0)
 print 'rank is', rank, name
 print 'data is', rank, x[rank], x[rank]['name'].nbytes, x[rank]['grades'].nbytes
 
+#perform some calculation on each processor
+x[rank]['grades'] += 1
+
+#define an array that will gather all of the data 
 if rank == 0:
     x_all = np.zeros((comm.size,), dtype = x.dtype)
     print rank, x_all.nbytes
 else:
     x_all = None
+
+#gather all of the data on the master node
 comm.Gather([x[rank], mpidt], [x_all, mpidt], root=0) # specify messages as [numpy_array, mpi_datatype]
 if rank == 0:
-    print x_all
+    print 'on rank ', rank, ', x_all = ', x_all
 
