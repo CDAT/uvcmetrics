b072128a914b5d2b69ca3085c4cbccc651721aad
mcenerney1 <mcenerney1@mcenerney1ml.llnl.gov>
2015-11-05 13:07:24 -0800

    Added numpy arrays for mpi slurm testing.

--- src/python/mpi_examples/mpiSlurmTest.py
+++ src/python/mpi_examples/mpiSlurmTest.py
@@ -1,10 +1,24 @@
 from mpi4py import MPI
-import sys, socket
+import sys, socket, numpy
 
+print sys.argv
+np = int(sys.argv[1])
+nd = int(sys.argv[2])
+print np, nd
 
+master = 0
 comm = MPI.COMM_WORLD
 size = comm.size
 rank = comm.rank
-
-line ='hello world from %s on rank %i' % (socket.gethostname(), rank)
+data = []
+if rank is 0:
+    data = numpy.random.rand(40)
+    data.shape = np, nd
+    print rank, data
+localdata = comm.scatter(data, root=master)
+line ='from %s on rank %i, the local data is ' % (socket.gethostname(), rank), localdata
 print line 
+
+collectedData = comm.gather(localdata.sum(), root=master)
+if rank is master:
+    print 'rank is ', rank, collectedData
