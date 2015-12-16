a16e1e922b716c88b9ffd19a67bea4253074a757
mcenerney1 <mcenerney1@llnl.gov>
2015-12-16 14:53:52 -0800

    Create cdscanfix.

---
+++ src/python/fileio/cdscanFix.py
@@ -0,0 +1,29 @@
+def cdscanFix():
+    """The purpose of this function is to make sure that the 
+    path to cdscan is in sys.path.  Also because of python it
+    must appear as cdscan.py in order to import cdscan."""
+    import sys, os, shutil
+    ospath = os.environ['PATH'].split(':')
+    #print ospath
+    
+    cdscanPath = None
+    for path in ospath:
+        #print path
+        dirList = os.listdir(path)
+        if 'cdscan' in dirList:
+            cdscanPath = path
+            if 'cdscan.py' not in dirList:
+                #make a copy and put it there
+                try:
+                    shutil.copyfile(path+'/cdscan', path+'/cdscan.py')
+                    #print 'cdscan.py' in os.listdir(path)
+                except:
+                    pass
+            break
+    sys.path.insert(0, cdscanPath)
+    #print cdscanPath
+    #print cdscanPath in sys.path
+if __name__ == '__main__':
+    cdscanFix()
+    import cdscan
+    print dir(cdscan)
