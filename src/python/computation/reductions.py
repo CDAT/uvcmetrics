c221ab31f8bdb99d0f32c180320ed4f4841b4b88
mcenerney1 <mcenerney1@llnl.gov>
2015-12-16 11:18:00 -0800

    debug

--- src/python/computation/reductions.py
+++ src/python/computation/reductions.py
@@ -2697,7 +2697,7 @@ def run_cdscan( fam, famfiles, cache_path=None, COMM=None ):
         #wait until cdscan is done
         message = comm1.recv(source = MPI.ANY_SOURCE)
         proc_status = MPI.Status().Get_error()
-        print ">>>>after cdscan", COMM.rank, message
+        print ">>>>after cdscan", COMM.rank, message, os.path.getsize(xml_name)
     if proc_status!=0: 
         print "ERROR: cdscan terminated with",proc_status
         print 'This is usually fatal. Frequent causes are an extra XML file in the dataset directory'
