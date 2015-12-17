4e714aa2a2a8ee8b7d454f9aba9591e49fb71df2
mcenerney1 <mcenerney1@llnl.gov>
2015-12-17 10:41:24 -0800

    Cleanup cdscan fix.

--- src/python/fileio/cdscanFix.py
+++ src/python/fileio/cdscanFix.py
@@ -3,27 +3,25 @@ def cdscanFix():
     path to cdscan is in sys.path.  Also because of python it
     must appear as cdscan.py in order to import cdscan."""
     import sys, os, shutil
-    ospath = os.environ['PATH'].split(':')
-    #print ospath
-    
+
     cdscanPath = sys.prefix + '/bin/'
-    print cdscanPath
-    #for path in ospath:
-        #print path
+    #print cdscanPath
+
     dirList = os.listdir(cdscanPath)
     if 'cdscan' in dirList:
-        #cdscanPath = path
         if 'cdscan.py' not in dirList:
             #make a copy and put it there
             try:
                 shutil.copyfile(cdscanPath+'/cdscan', cdscanPath+'/cdscan.py')
-                print 'cdscan.py' in os.listdir(path)
+                #print 'cdscan.py' in os.listdir(path)
             except:
                 pass
-            #break
+    else:
+        print 'cdscan is not in ' + cdscanPath
+        sys.exit()
+        
     sys.path.insert(0, cdscanPath)
-    #print cdscanPath
-    #print cdscanPath in sys.path
+
 if __name__ == '__main__':
     cdscanFix()
     import cdscan
