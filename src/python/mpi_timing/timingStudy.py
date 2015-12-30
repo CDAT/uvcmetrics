d9b8f147bb55b26c4ffe18c55ecd9c08f87d2ab5
mcenerney1 <mcenerney1@llnl.gov>
2015-12-30 15:26:35 -0800

    debug

--- src/python/mpi_timing/timingStudy.py
+++ src/python/mpi_timing/timingStudy.py
@@ -1,4 +1,12 @@
 import subprocess, pdb, time
+config = [(1,1),(1,2),(1,3),(1,4),(1,6),(1,8),
+          (2,1),(2,2),(2,3),(2,4),(2,6),
+          (3,1),(3,2),(3,4),(3,8),
+          (4,1),(4,2),(4,3),(4,6),
+          (6,1),(6,2),(6,4),
+          (8,1),(8,3)]
+for (N,n) in config:
+    print 'sbatch --nodes=' + str(N) + ' --ntasks-per-node=' + str(n) + ' diag.sh'
 SBATCH_EXEC = 'sbatch --nodes=1 --ntasks-per-node=6 diag.sh'
 proc=subprocess.Popen([SBATCH_EXEC], shell=True, stdout=subprocess.PIPE)
 time.sleep(30)
