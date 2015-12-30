8dcf9306894b49bb40479ad467d8dbaf39a23cfc
mcenerney1 <mcenerney1@llnl.gov>
2015-12-30 11:09:11 -0800

    New files for timing study.

---
+++ src/python/mpi_timing/timingStudy.py
@@ -0,0 +1,20 @@
+import subprocess
+SBATCH_EXEC = 'sbatch --nodes=1 --ntasks-per-node=6 diag.sh'
+x=subprocess.Popen([SBATCH_EXEC], shell=True, stdout=subprocess.PIPE)
+
+#retrieve jobid and create the slurm file name
+msg = x.communicate()[0]
+msg = msg.split()
+jobid = msg[-1]
+slurmFile = 'slurm-'+jobid+'.out'
+print slurmFile
+
+f=open(slurmFile)
+for line in f.readlines():
+    if 'time =' in line:
+        print line
+        break
+#retrieve run time
+t=line.split()[2]
+time = float(t)
+print time
