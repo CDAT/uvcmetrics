77fbcdf92e5d3ce1b3a000f60a9f1254b098f32b
mcenerney1 <mcenerney1@llnl.gov>
2015-12-30 13:47:47 -0800

    debug

--- src/python/mpi_timing/timingStudy.py
+++ src/python/mpi_timing/timingStudy.py
@@ -1,8 +1,9 @@
-import subprocess, pdb
+import subprocess, pdb, time
 SBATCH_EXEC = 'sbatch --nodes=1 --ntasks-per-node=6 diag.sh'
 proc=subprocess.Popen([SBATCH_EXEC], shell=True, stdout=subprocess.PIPE)
+time.sleep(30)
 #pdb.set_trace()
-subprocess.Popen.wait(proc) #x.wait()
+#subprocess.Popen.wait(proc) #x.wait()
 #retrieve jobid and create the slurm file name
 msg = proc.communicate()[0]
 msg = msg.split()
