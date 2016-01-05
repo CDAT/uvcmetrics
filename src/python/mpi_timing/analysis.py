45866cca2db8b1cbe335d424f07ec02aac198351
mcenerney1 <mcenerney1@llnl.gov>
2016-01-05 14:04:58 -0800

    app to analyze data

---
+++ src/python/mpi_timing/analysis.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+f=open('timing.dat')
+titles = f.readline()
+
+for line in f.readline():
+    line = line.split()
+    Nnodes = line[0]
+    Ntasks = line[1]
+    
+    data = []
+    for x in line[2:]:
+        data += [float(x)]
+        
+    data = np.array(data)
+    print Nnodes, Ntasks, data.mean(), data.var()
