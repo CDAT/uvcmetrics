6bd2ee6cbe0a3f02636af1cadf0670e7da1a14eb
mcenerney1 <mcenerney1@llnl.gov>
2016-01-05 08:24:47 -0800

    Change to single line call to mpirun

--- src/python/mpi_timing/diag.sh
+++ src/python/mpi_timing/diag.sh
@@ -6,6 +6,4 @@
 
 source /opt/nfs/mcenerney1/11_03_15/bin/setup_runtime.sh
 
-mpirun  python /opt/nfs/mcenerney1/uvcmetrics/src/python/frontend/diags.py --model path=/opt/nfs/mcenerney1//uvcmetrics_test_data/cam_output/,climos=ye
-s --obs path=/opt/nfs/mcenerney1/uvcmetrics_test_data/obs_atmos/,filter='f_startswith("c_t_NCEP")',climos=yes --outputdir /opt/nfs/mcenerney1/diagout/ 
---package AMWG --sets 8 --seasons ANN --plots yes --vars T --parallel --cachepath /opt/nfs/mcenerney1/tmp/
+mpirun  python /opt/nfs/mcenerney1/uvcmetrics/src/python/frontend/diags.py --model path=/opt/nfs/mcenerney1//uvcmetrics_test_data/cam_output/,climos=yes --obs path=/opt/nfs/mcenerney1/uvcmetrics_test_data/obs_atmos/,filter='f_startswith("c_t_NCEP")',climos=yes --outputdir /opt/nfs/mcenerney1/diagout/ --package AMWG --sets 8 --seasons ANN --plots yes --vars T --parallel --cachepath /opt/nfs/mcenerney1/tmp/
