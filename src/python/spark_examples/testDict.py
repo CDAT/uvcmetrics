4417bb58405171b2922599ca41379bb06b369a6d
mcenerney1 <mcenerney1@llnl.gov>
2016-01-07 15:56:58 -0800

    debug

--- src/python/spark_examples/testDict.py
+++ src/python/spark_examples/testDict.py
@@ -27,8 +27,8 @@ if __name__ == "__main__":
         i += 1
     Max = {}
     def update(S, T):
-        k,v = T[-1]
-        Max[k] = v        
+        print T[-1]
+        #Max[k] = v        
         
     MAXs  = sc.parallelize(data.keys(), partitions).\
             map(lambda key: (key, data[key].compute()) ).\
