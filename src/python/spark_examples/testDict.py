f26f0afcbf7ace700a728f6082b8dfdbc86886d5
mcenerney1 <mcenerney1@llnl.gov>
2016-01-08 14:33:14 -0800

    debug

--- src/python/spark_examples/testDict.py
+++ src/python/spark_examples/testDict.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     """
     sc = SparkContext(appName="Dictionary Test")
     partitions = int(sys.argv[1])
-
+    print 'host = ',socket.gethostname()
     import string
     data = dict.fromkeys(string.ascii_lowercase, 0)
       
@@ -26,15 +26,10 @@ if __name__ == "__main__":
         data[key] = X(np.array(range(i), dtype=float))
         print data[key].x
         i += 1
-    print data
         
     P = sc.parallelize(data.keys(), partitions)
     M = P.map(lambda key: (key, data[key].compute()) )
     MAXs = M.reduceByKey( lambda x: x )
         
-    print dict(MAXs.collect()) #), type(MAXs)
-    #print Max
-    #for m in MAXs:
-    #    print m, '\n'
-
+    print dict(MAXs.collect()) 
     sc.stop()
