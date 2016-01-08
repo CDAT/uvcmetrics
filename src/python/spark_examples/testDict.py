b3c4c774831318c414a21dbd2b60f4252b6e887d
mcenerney1 <mcenerney1@llnl.gov>
2016-01-08 13:51:15 -0800

    debug

--- src/python/spark_examples/testDict.py
+++ src/python/spark_examples/testDict.py
@@ -19,25 +19,13 @@ if __name__ == "__main__":
 
     import string
     data = dict.fromkeys(string.ascii_lowercase, 0)
-    
-    
+      
     i = 1
     for key in data.keys():
         data[key] = X(np.array(range(i), dtype=float))
+        print data[key].x
         i += 1
     print data
-    
-    
-    Max = {}
-    def update(S, T):
-        print 'in update', '\n S=', S, '\n T=', T#T[-1]
-        try:
-            k, v = T
-            Max[k] = v
-        except:
-            if type(S) is type({}):
-                Max = dict(S.items() + T.items())
-        return Max    
         
     P = sc.parallelize(data.keys(), partitions)
     M = P.map(lambda key: (key, data[key].compute()) )
