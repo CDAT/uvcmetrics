53df73ddb56a41567f9f41eadf5026b7ac4b5e7b
mcenerney1 <mcenerney1@llnl.gov>
2016-01-11 15:37:20 -0800

    debug

--- src/python/spark_examples/testDict.py
+++ src/python/spark_examples/testDict.py
@@ -2,7 +2,6 @@ import sys, socket
 from random import random
 from operator import add
 import numpy as np
-import cdms2
 
 from pyspark import SparkContext
 class X(object):
@@ -12,25 +11,21 @@ class X(object):
         print ('host = ' + socket.gethostname())
         return self.x.max()
 
-if __name__ == "__main__":
-    """
-        Usage: pi [partitions]
-    """
-    sc = SparkContext(appName="Dictionary Test")
-    partitions = int(sys.argv[1])
+sc = SparkContext(appName="Dictionary Test")
+partitions = int(sys.argv[1])
 
-    import string
-    data = dict.fromkeys(string.ascii_lowercase, 0)
-      
-    i = 1
-    for key in data.keys():
-        data[key] = X(np.array(range(i), dtype=float))
-        print data[key].x
-        i += 1
-        
-    P = sc.parallelize(data.keys(), partitions)
-    M = P.map(lambda key: (key, data[key].compute()) )
-    MAXs = M.reduceByKey( lambda x: x )
-        
-    print dict(MAXs.collect()) 
-    sc.stop()
+import string
+data = dict.fromkeys(string.ascii_lowercase, 0)
+  
+i = 1
+for key in data.keys():
+    data[key] = X(np.array(range(i), dtype=float))
+    print data[key].x
+    i += 1
+    
+P = sc.parallelize(data.keys(), partitions)
+M = P.map(lambda key: (key, data[key].compute()) )
+MAXs = M.reduceByKey( lambda x: x )
+    
+print dict(MAXs.collect()) 
+sc.stop()
