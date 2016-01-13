37dde6392002208246353e75f850cf2bbdbd367b
mcenerney1 <mcenerney1@llnl.gov>
2016-01-13 07:40:57 -0800

    create data

--- src/python/spark_examples/createDummyData.py
+++ src/python/spark_examples/createDummyData.py
@@ -1,14 +1,21 @@
 import cdms2
-import string
+import string, cdutil
 import numpy as np
 
 varids=string.ascii_lowercase
 
 f=cdms2.open('testData.nc', 'w')
-i=0
+n=0
 for varid in varids:
-    a = np.array(range(i), dtype=float)
-    var = cdms2.createVariable(a.std())
+    a = -1*np.arange(n, dtype=float)
+    var = cdms2.createVariable(a)
     var.id = varid
+    var.units = 'peas'
+    T = cdms2.createAxis(numpy.arange(n, dtype='d'))
+    T.designateTime()
+    T.id="time"
+    T.units = "months"
+    cdutil.times.setTimeBoundsMonthly(T)
+    var.setAxis(0, T)
     f.write(var)
     
