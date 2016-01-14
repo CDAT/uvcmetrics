be84182f0309e054e03fdfbd4b86598a351d30ac
mcenerney1 <mcenerney1@llnl.gov>
2016-01-14 09:41:36 -0800

    change test data

--- src/python/spark_examples/createDummyData.py
+++ src/python/spark_examples/createDummyData.py
@@ -2,11 +2,12 @@ import cdms2
 import string, cdutil, pdb
 import numpy as np
 
-varids=string.ascii_lowercase
-
+#varids=string.ascii_lowercase
+data = dict.fromkeys(string.ascii_lowercase, 0)
+print data.keys()
 f=cdms2.open('testData.nc', 'w')
 n=1
-for varid in varids:
+for varid in data.keys():
     a = -1*np.arange(1, n+1, dtype=float)
     var = cdms2.createVariable(a)
     var.id = varid
