1d9308e5a4c5f185a18d8d3d865273dfa2e89f37
mcenerney1 <mcenerney1@llnl.gov>
2016-01-28 14:23:38 -0800

    spark timing study

--- src/python/frontend/uvcdat.py
+++ src/python/frontend/uvcdat.py
@@ -1067,8 +1067,6 @@ class plot_spec(object):
             print 'partitions = ', partitions
             P = sc.parallelize(self.reduced_variables.keys(), partitions)
             RESULTS = P.map( lambda key: (key, self.reduced_variables[key].reduce(None, RETURN_ARRAYS=True))  ) 
-            import time
-            time.sleep(20)
             RESULTS = dict(RESULTS.collect())
             sc.stop()
             
