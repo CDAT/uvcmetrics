de61490aa9ffbde0a46ce09975919c8cb1f7253d
mcenerney1 <mcenerney1@llnl.gov>
2016-02-01 11:45:40 -0800

    nfs debug

--- src/python/mpi_timing/test.sh
+++ src/python/mpi_timing/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source $NFSHOME/11_03_15/bin/setup_runtime.sh
 echo $SLURM_JOB_NUM_NODES $SLURM_NTASKS_PER_NODE
 #mpirun python -c "print 'hello' "
 mpirun $NFSHOME/uvcmetrics/src/python/mpi_examples/cdms2issue.py
