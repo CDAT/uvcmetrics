a89e61c21b44044576869634a5330b5c2043842b
mcenerney1 <mcenerney1@llnl.gov>
2016-02-02 08:20:05 -0800

    nfs debug

--- src/python/mpi_examples/cdms2issue.py
+++ src/python/mpi_examples/cdms2issue.py
@@ -19,7 +19,7 @@ cdms2.setNetcdfUseParallelFlag(0)
 if rk == 0:
     #pdb.set_trace()
     f = cdms2.open( '~/uvcmetrics_test_data/obs_atmos/c_t_NCEP_01_climo.nc', 'r' )
-    print rk, socket.hostname(), f.variables, '\n'
+    print rk, socket.gethostname(), f.variables, '\n'
 else:
     f = cdms2.open( '~/uvcmetrics_test_data/obs_atmos/c_t_NCEP_02_climo.nc', 'r' )
-    print rk, socket.hostname(), f.variables, '\n' #'nothing to do'
+    print rk, socket.gethostname(), f.variables, '\n' #'nothing to do'
