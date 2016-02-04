86f899bbd77bbf29c6a2c5f6d0f0f0f971ff8868
mcenerney1 <mcenerney1@llnl.gov>
2016-02-04 13:13:21 -0800

    spark analysis

---
+++ src/python/spark_timing/analysis.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+fin=open('timing.dat')
+titles = fin.readline()
+
+fout = open('results.dat', 'w')
+
+for line in fin.readlines():
+    line = line.split()
+    Nnodes = line[0]
+    Ntasks = line[1]
+    Nparts = line[2]
+    
+    data = []
+    for x in line[3:]:
+        data += [float(x)]
+        
+    data = np.array(data)
+    mean, var = data.mean(), data.var()
+    
+    output = Nnodes + ' ' + Ntasks + ' ' + ' ' Npart + str(mean) + ' ' + str(var) + '\n'
+    print output
+    
+    fout.write(output)
