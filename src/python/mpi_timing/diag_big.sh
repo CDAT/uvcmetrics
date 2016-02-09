9e322db143bb5f3d2e97bc9607606d390232d29b
mcenerney1 <mcenerney1@llnl.gov>
2016-02-09 13:14:48 -0800

    trying large data with mpi

--- src/python/mpi_timing/diag_big.sh
+++ src/python/mpi_timing/diag_big.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=diag_8 --time=01:00:00
 #SBATCH -D /opt/nfs/mcenerney1/mpi_output/big/
-#SBATCH --exclude=greyworm2,greyworm7,greyworm5
+#SBATCH --exclude=greyworm2,greyworm7
 #hostname
 
 rm $NFSHOME/tmp/*
@@ -11,7 +11,7 @@ source $NFSHOME/11_03_15/bin/setup_runtime.sh
 
 mpirun  python $NFSHOME/uvcmetrics/src/python/frontend/diags.py \
 --model path=/cmip5_css02/data/cmip5/output1/MIROC/MIROC5/piControl/mon/atmos/Amon/r1i1p1/hur/1/,climos=no \
---obs path=/cmip5_css02/data/cmip5/output1/MIROC/MIROC5/piControl/mon/atmos/Amon/r1i1p1/hur/1/,climos=no \
+--obs   path=$NFSHOME/uvcmetrics_test_data/obs_atmos/,filter='f_startswith("c_t_NCEP")',climos=yes  \
 --outputdir $NFSHOME/diagout/ \
 --package AMWG --sets 8 --seasons ANN --plots yes --vars hur --parallel \
 --cachepath $NFSHOME/tmp/
