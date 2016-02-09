2801e262afd8ff77c626863cb9c59f49d2bd44ff
mcenerney1 <mcenerney1@llnl.gov>
2016-02-09 14:26:37 -0800

    trying large data with mpi

--- src/python/mpi_timing/diag_big.sh
+++ src/python/mpi_timing/diag_big.sh
@@ -10,10 +10,8 @@ rm $NFSHOME/tmp/*
 source $NFSHOME/11_03_15/bin/setup_runtime.sh
 
 mpirun  python $NFSHOME/uvcmetrics/src/python/frontend/diags.py \
---model path=$NFSHOME/uvcmetrics_test_data/cam_output/,climos=yes \
---obs path=$NFSHOME/uvcmetrics_test_data/obs_atmos/,filter='f_startswith("c_t_NCEP")',climos=yes \
+--model path=/cmip5_css02/data/cmip5/output1/MIROC/MIROC5/piControl/mon/atmos/Amon/r1i1p1/hur/1/,climos=no \
+--obs   path=/cmip5_css02/data/cmip5/output1/MIROC/MIROC5/piControl/mon/atmos/Amon/r1i1p1/hur/1/,climos=no, \
 --outputdir $NFSHOME/diagout/ \
---package AMWG --sets 8 --seasons ANN --plots yes --vars T --parallel \
+--package AMWG --sets 8 --seasons ANN --plots yes --vars hur --parallel \
 --cachepath $NFSHOME/tmp/
-
-#/cmip5_css02/data/cmip5/output1/MIROC/MIROC5/piControl/mon/atmos/Amon/r1i1p1/hur/1/,climos=no
