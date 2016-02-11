d18438bc034b82ba25b87ece9d40d32e512eecb1
mcenerney1 <mcenerney1@llnl.gov>
2016-02-11 09:43:12 -0800

    trying large data with mpi

--- src/python/mpi_timing/diag_big.sh
+++ src/python/mpi_timing/diag_big.sh
@@ -4,7 +4,7 @@
 #SBATCH -D /opt/nfs/mcenerney1/mpi_output/big/
 #SBATCH --output=mpi_run
 #SBATCH --exclude=greyworm1,greyworm2,greyworm7
-#SBATCH --mem=100MB
+#SBATCH --mem=150MB
 
 rm $NFSHOME/tmp/*
 
