ac216ab03eac762c72b431dd7e79dc803a132b7d
mcenerney1 <mcenerney1@llnl.gov>
2016-02-18 10:29:28 -0800

    debug mpi timing data

--- src/python/mpi_timing/mpiTimingData.py
+++ src/python/mpi_timing/mpiTimingData.py
@@ -13,7 +13,7 @@ for file in listing:
 timing_data = 'Nnodes  Ntasks  runs '
 old_Nnodes, old_Ntasks = None, None
 for slurmFile in listing:
-    Nnodes, Ntasks, run = slurmFile.split('_')[:]
+    Nnodes, Ntasks, run = slurmFile.split('_')[1:]
     
     g=open(SLURM_OUTPUTDIR + slurmFile)
     for line in g.readlines():
