caf86c02065ac66426039ddf1ad42ed06964f788
mcenerney1 <mcenerney1@llnl.gov>
2016-02-18 10:23:12 -0800

    debug mpi timing data

--- src/python/mpi_timing/mpiTimingData.py
+++ src/python/mpi_timing/mpiTimingData.py
@@ -28,7 +28,7 @@ for slurmFile in listing:
     t=float(t)
     #pdb.set_trace()
     if not (Nnodes, Ntasks) == (old_Nnodes, old_Ntasks):
-        timing_data += str(Nnodes) + '       ' + str(Ntasks) + '       '
+        timing_data += '\n' + str(Nnodes) + '       ' + str(Ntasks) + '       '
         old_Nnodes, old_Ntasks = Nnodes, Ntasks
     timing_data += str(round(t,2)) + ' '
     print slurmFile, t
