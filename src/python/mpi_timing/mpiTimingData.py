686515826bdc9f22d5978f7f850ad485aa8adccd
mcenerney1 <mcenerney1@llnl.gov>
2016-02-18 10:11:33 -0800

    timing study for mpi

--- src/python/mpi_timing/mpiTimingData.py
+++ src/python/mpi_timing/mpiTimingData.py
@@ -26,7 +26,7 @@ for slurmFile in listing:
     #retrieve run time
     t=line.split('=')[1]
     pdb.set_trace()
-    if not (Nnodes, Ntasks == old_Nnodes, old_Ntasks):
+    if not (Nnodes, Ntasks) == (old_Nnodes, old_Ntasks):
         timing_data += str(Nnodes) + '       ' + str(Ntasks) + '       '
         old_Nnodes, old_Ntasks = Nnodes, Ntasks
     timing_data += t[0:6] + ' '
