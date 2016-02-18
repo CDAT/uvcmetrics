e7ff26c9cf6720ef9922b8ca7619ea288ce491ff
mcenerney1 <mcenerney1@llnl.gov>
2016-02-18 08:28:58 -0800

    timing study for mpi

--- src/python/mpi_timing/timingStudy.py
+++ src/python/mpi_timing/timingStudy.py
@@ -4,17 +4,17 @@ DIRSIZE = sys.argv[1]
 NFSHOME = os.environ['NFSHOME']
 TIMING_PATH = NFSHOME + '/uvcmetrics/src/python/mpi_timing/'
 SLURM_OUTPUTDIR = NFSHOME + '/slurm_output/' + DIRSIZE + '/'
-nruns = 1
+nruns = 5
 
 for (N,n) in config:
     SBATCH_EXEC = 'sbatch --nodes=' + str(N) + ' --ntasks-per-node=' + str(n) + ' ' + TIMING_PATH +'diag_big.sh'
     print SBATCH_EXEC
     for run in range(nruns):
         #SBATCH_EXEC = 'sbatch --nodes=1 --ntasks-per-node=6 diag.sh'
-        slurm_output_file = SLURM_OUTPUTDIR + 'slurm_run_'+ str(N) +'_' + str(n) + '_' + str(run)
+        slurm_output_file = SLURM_OUTPUTDIR + 'run_'+ str(N) +'_' + str(n) + '_' + str(run)
         os.environ['SLURMOUTPUT'] = slurm_output_file
         proc=subprocess.Popen([SBATCH_EXEC], shell=True, stdout=subprocess.PIPE)
-        time.sleep(15) #15 minutes
+        time.sleep(15*60) #15 minutes
         #pdb.set_trace()
         #subprocess.Popen.wait(proc) #x.wait()
         #retrieve jobid and create the slurm file name
