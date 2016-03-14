81e24fcaf36b5e91aec6115ee841c8ea7b4a0f32
zshaheen <zshaheen93@gmail.com>
2016-03-14 14:54:03 -0700

    Done with /fileio/

--- src/python/fileio/stationData.py
+++ src/python/fileio/stationData.py
@@ -1,11 +1,11 @@
-import pdb
+import pdb, logging
 class stationData():
     """ This class support the RAOBS.nc file used for plot set 12. It reads the file and
     sets up the variables needed later for analysis."""
     def __init__(self, obsDataFile):
         import cdms2
         if obsDataFile.find('RAOBS.nc')<0:
-            print "INFO: We usually get station data from RAOBS.nc.  This data file is",obsDataFile
+            print "We usually get station data from RAOBS.nc.  This data file is",obsDataFile
         f=cdms2.open(obsDataFile)
         self.KEYS = f.listvariables() 
         self.KEYS = [ a for a in self.KEYS if a!='STATIONS' and a!= 'MONTHS']
@@ -55,5 +55,5 @@ class stationData():
             return data
             #return self.data[dataId][0][stationIndex][month], self.data[dataId][1]
         except:
-            print 'no data for ' + dataId + ' and station ' + str(stationIndex)
+            logging.exception('no data for %s and station %s', dataId, str(stationIndex))
             return None
