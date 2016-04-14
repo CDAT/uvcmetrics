b6937dbbb10214e37803e2e86a82ad3e62d0653b
Jeff Painter <painter1@llnl.gov>
2016-04-14 14:42:50 -0700

    In reduce(), check whether reduced_data is None before setting its attributes.

--- src/python/computation/reductions.py
+++ src/python/computation/reductions.py
@@ -2660,7 +2660,8 @@ def run_cdscan( fam, famfiles, cache_path=None ):
         print "ERROR: cdscan terminated with",proc_status
         print 'This is usually fatal. Frequent causes are an extra XML file in the dataset directory'
         print 'or non-CF compliant input files'
-        raise Exception("cdscan failed - %s" %cdscan_line)
+        #raise Exception("cdscan failed - %s" %cdscan_line)
+        return None
     return xml_name
 
 def join_data(*args ):
@@ -2995,12 +2996,14 @@ class reduced_variable(ftrow,basic_id):
                 raise Exception
             if reduced_data is not None and type(reduced_data) is not list:
                 reduced_data._vid = vid
-            reduced_data.weighting = weighting  # needed if we have to average it further later on
+            if reduced_data is not None:
+                reduced_data.weighting = weighting  # needed if we have to average it further later on
             f.close()
         if hasattr(reduced_data,'mask') and reduced_data.mask.all():
             reduced_data = None
-        reduced_data._filename = self._filename
-        reduced_data.filetable = self.filetable
+        if reduced_data is not None:
+            reduced_data._filename = self._filename
+            reduced_data.filetable = self.filetable
         return reduced_data
 
 class rv(reduced_variable):
