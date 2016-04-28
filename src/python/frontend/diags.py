374cf0cdedd054ddf8ef97ae9a8ae38c7716412a
mcenerney1 <mcenerney1@llnl.gov>
2016-04-28 07:08:50 -0700

    Issues with diags, arguments to customizeTemplate seem to be wrong

--- src/python/frontend/diags.py
+++ src/python/frontend/diags.py
@@ -679,7 +679,7 @@ def makeplots(res, vcanvas, vcanvas2, varid, fname, plot, package):
                     
                     if hasattr(plot, 'customizeTemplates'):
                         tm, tm2 = plot.customizeTemplates( [(vcanvas, tm), (vcanvas2, tm2)],
-                                                           varIndex, rsr.presentation, var=var )
+                                                           varIndex=varIndex, graphicMethod=rsr.presentation, var=var )
                     # Single plot                    
                     plot.vcs_plot(vcanvas, var, rsr.presentation, tm, bg=1,
                                   title=title, source=rsr.source,
