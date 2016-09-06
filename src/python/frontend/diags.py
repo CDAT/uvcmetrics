3a5f5b7ecdd5de15fdcb09c3e80d14dc3e9d92b9
mcenerney1 <mcenerney1@llnl.gov>
2016-09-06 15:42:23 -0700

    debug

--- src/python/frontend/diags.py
+++ src/python/frontend/diags.py
@@ -754,6 +754,7 @@ def makeplots(res, vcanvas, vcanvas2, varid, frname, plot, package, displayunits
 if __name__ == '__main__':
     print "UV-CDAT Diagnostics, command-line version"
     print ' '.join(sys.argv)
+    pdb.set_trace()
     o = Options()
     o.parseCmdLine()
     o.verifyOptions()
