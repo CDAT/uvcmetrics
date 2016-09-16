d5733dc2671999a46dab3fa50cdb42dc913cede4
mcenerney1 <mcenerney1@llnl.gov>
2016-09-16 07:10:36 -0700

    scale TGCLDLWP to g/m2

--- src/python/frontend/diags.py
+++ src/python/frontend/diags.py
@@ -341,7 +341,7 @@ def run_diags( opts ):
                                                plotparms = { 'model':{'levels':opts['levels'], 'colormap':opts['colormaps']['model']},
                                                              'obs':{'levels':opts['levels'], 'colormap':opts['colormaps']['obs']},
                                                              'diff':{'levels':opts['difflevels'], 'colormap':opts['colormaps']['diff']} } )
-                        pdb.set_trace()
+                        
                         # Do the work (reducing variables, etc)
                         res = plot.compute(newgrid=-1) # newgrid=0 for original grid, -1 for coarse
                         # typically res is a list of uvc_simple_plotspec.  But an item might be a tuple.
@@ -671,7 +671,7 @@ def makeplots(res, vcanvas, vcanvas2, varid, frname, plot, package, displayunits
                 else:
                     # Reverting to having an update otherwise we get double titles
                     vcanvas2.setcolormap('bl_to_darkred')
-
+                    pdb.set_trace()
                     #check for units specified for display purposes
                     var_save = var.clone()
                     if displayunits != None:
