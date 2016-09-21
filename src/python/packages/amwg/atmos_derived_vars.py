2f14033e108e4073697d41ad6ce0d0df60f1157d
mcenerney1 <mcenerney1@llnl.gov>
2016-09-21 13:24:13 -0700

    debug TGCLDLWP with NVAP

--- src/python/packages/amwg/atmos_derived_vars.py
+++ src/python/packages/amwg/atmos_derived_vars.py
@@ -238,7 +238,7 @@ common_derived_variables = {
         ],
     #TGCLDLWP_OCEAN
     'TGCLDLWP':[derived_var(
-            vid='TGCLDLWP', inputs=['TGCLDLWP', 'OCNFRAC'], outputs=['TGCLDLWP'],
+            vid='TGCLDLWP', inputs=['TGCLDLWP', 'LANDFRAC'], outputs=['TGCLDLWP'],
             func=(lambda x, y, units='g/m^2': simple_vars.ocean_only(x,y, units)) ),
             derived_var(
             vid='TGCLDLWP', inputs=['TGCLDLWP_OCEAN'], outputs=['TGCLDLWP'],
