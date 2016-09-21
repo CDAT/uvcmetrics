458e9d5488a7dbc1a6932c560b040f8c23d3d8a3
mcenerney1 <mcenerney1@llnl.gov>
2016-09-21 14:27:41 -0700

    debug TGCLDLWP with NVAP

--- src/python/packages/amwg/atmos_derived_vars.py
+++ src/python/packages/amwg/atmos_derived_vars.py
@@ -237,11 +237,11 @@ common_derived_variables = {
                     clmisr, h0,h1, t0,t1) ) )
         ],
     #TGCLDLWP_OCEAN
-    'TGCLDLWP':[derived_var(
+    'TGCLDLWP_OCN':[derived_var(
             vid='TGCLDLWP', inputs=['TGCLDLWP_OCEAN'], outputs=['TGCLDLWP_OCN'],
             func=(lambda x: convert_units(x, 'g/m^2')) ), 
                 derived_var(
-            vid='TGCLDLWP', inputs=['TGCLDLWP', 'OCNFRAC'], outputs=['TGCLDLWP_OCN'],
+            vid='TGCLDLWP_OCN', inputs=['TGCLDLWP', 'OCNFRAC'], outputs=['TGCLDLWP_OCN'],
             func=(lambda x, y, units='g/m^2': simple_vars.ocean_only(x,y, units)) )],
     #...end of clouds, Yuying Zhang
 
