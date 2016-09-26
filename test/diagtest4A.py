5aa7d65071fa113e873030ceacb4bc0e18e1b39d
Jeff Painter <painter1@llnl.gov>
2016-09-26 13:37:07 -0700

    Update baselines for plot set 4A because of a correction to the mean calculation, and better soruce fields in the plot; and different variable names in the NetCDF file.

--- test/diagtest4A.py
+++ test/diagtest4A.py
@@ -29,8 +29,8 @@ dt = diags_test.DiagTest( modeldir, obsdir, plotset, filterid, obsid, varid, sea
 imagefilename = 'figure-set4A_Global_ANN_T_plot-combined.png'
 imagethreshold = None
 ncfiles = {}
-ncfiles['figure-set4A_Global_ANN_T_plot--model.nc'] = ['dv_T_levlon_ANN_ft1_cam35_data_smaller']
-ncfiles['figure-set4A_Global_ANN_T_plot--obs.nc'] = ['rv_T_ANN_ft2_obs_NCEP']
+ncfiles['figure-set4A_Global_ANN_T_plot--model.nc'] = ['dv_T_levlon_ANN_ft1_cam35_data_smaller_model']
+ncfiles['figure-set4A_Global_ANN_T_plot--obs.nc'] = ['rv_T_ANN_ft2_NCEP']
 
 # Test of NetCDF data (nc) file match:
 rtol = 1.0e-3
