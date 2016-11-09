deb726ae3bf566a625e12d739c5c13379387e282
Jeff Painter <painter1@llnl.gov>
2016-11-09 14:23:50 -0800

    change file names for plot set 13 testing

--- test/diagtest13.py
+++ test/diagtest13.py
@@ -20,13 +20,13 @@ dt = diags_test.DiagTest( modeldir, obsdir, plotset, filterid, obsid, varid, sea
 
 # Test of graphics (png) file match:
 # This just looks at combined plot, aka summary plot, which is a compound of three plots.
-imagefilename = 'figure-set13_Global_ANN_CLISCCP_plot-combined.png'
+imagefilename = 'set13_ANN_CLISCCP-combined.png'
 imagethreshold = None
 ncfiles = {}
 #ncfiles['CLISCCP_ANN_Global_(1)_cam35_data_smaller.nc'] = ['dv_CLISCCP_ANN_ft1_cam35_data_smaller']
 #ncfiles['CLISCCP_ANN_Global_(2)_obs_data_13_ISCCPCOSP.nc'] = ['rv_CLISCCP_ANN_Global_ft2_obs_data_13_ISCCPCOSP']
-ncfiles['figure-set13_Global_ANN_CLISCCP_plot--model.nc'] = ['dv_CLISCCP_ANN_ft1_cam35_data_smaller_model']
-ncfiles['figure-set13_Global_ANN_CLISCCP_plot--obs.nc'] = ['rv_CLISCCP_ANN_Global_ISCCPCOSP']
+ncfiles['set13_ANN_CLISCCP--model.nc'] = ['dv_CLISCCP_ANN_ft1_cam35_data_smaller_model']
+ncfiles['set13_ANN_CLISCCP--obs.nc'] = ['rv_CLISCCP_ANN_Global_ISCCPCOSP']
 
 # Test of NetCDF data (nc) file match:
 rtol = 1.0e-3
